cf7551fdfbc99c2b1ee6fff434e1faa03ad65b2a
Michael Freeman <mfreeman451@gmail.com>
2020-12-10 17:55:26 -0600

    cleanup

--- OLD-lib/utils/datetime.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-DateTime parseTime(dynamic date) {
-  return Platform.isIOS ? (date as Timestamp).toDate() : (date as DateTime);
-}
