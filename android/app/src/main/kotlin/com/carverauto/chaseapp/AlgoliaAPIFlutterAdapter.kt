2e35e1984cc745925af6d80e154506ce4bdaed24
mfreeman451 <mfreeman451@gmail.com>
2021-12-20 02:56:46 -0600

    Adding Algolia..unfinished

---
+++ android/app/src/main/kotlin/com/carverauto/chaseapp/AlgoliaAPIFlutterAdapter.kt
@@ -0,0 +1,29 @@
+class AlgoliaAPIFlutterAdapter(applicationID: ApplicationID, apiKey: APIKey) {
+
+    val client = ClientSearch(applicationID, apiKey)
+
+    fun perform(call: MethodCall, result: MethodChannel.Result): Unit = runBlocking {
+        Log.d("AlgoliaAPIAdapter", "method: ${call.method}")
+        Log.d("AlgoliaAPIAdapter", "args: ${call.arguments}")
+        val args = call.arguments as? List<String>
+        if (args == null) {
+            result.error("AlgoliaNativeError", "Missing arguments", null)
+            return@runBlocking
+        }
+
+        when (call.method) {
+            "search" -> search(indexName = args[0].toIndexName(), query = Query(args[1]), result = result)
+            else -> result.notImplemented()
+        }
+    }
+
+    suspend fun search(indexName: IndexName, query: Query, result: MethodChannel.Result) {
+        val index = client.initIndex(indexName)
+        try {
+            val search = index.search(query = query)
+            result.success(Json.encodeToString(ResponseSearch.serializer(), search))
+        } catch (e: Exception) {
+            result.error("AlgoliaNativeError", e.localizedMessage, e.cause)
+        }
+    }
+}
