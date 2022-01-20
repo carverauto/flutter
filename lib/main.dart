c291fdaaa31b0524a96dcfcad8389276e0635898
rutvik110 <takrutvik@gmail.com>
2022-01-20 09:33:08 +0530

    creating sharedPreferance Singleton on start

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:chaseapp/src/root.dart';
 import 'package:chaseapp/src/top_level_providers/nodle_provider.dart';
+import 'package:chaseapp/src/top_level_providers/services_providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
@@ -18,7 +20,12 @@ void main() async {
   // Setting up const/singletons like this will be redundant after refactoring
   // Prefs.init();
   // Initialize other services like SharedPreferances, etc and provide through providers
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
   runApp(ProviderScope(
+    overrides: [
+      sharedPreferancesProvider.overrideWithValue(prefs),
+    ],
+    observers: [],
     child: MyApp(),
   ));
 }
