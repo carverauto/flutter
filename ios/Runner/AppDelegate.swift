1e5332d2b7f738c174613efc349703c55b271285
Michael Freeman <mfreeman@deathstar.localdomain>
2022-01-19 20:36:07 -0600

    Added swift code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,6 +6,11 @@ import NodleSDK
 import SQLite
 import CoreLocation
 import CoreBluetooth
+import simd
+import grpc
+
+//let nodle = Nodle.sharedInstance
+private var nodle: Nodle?
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,9 +18,29 @@ import CoreBluetooth
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+      let nodleChannel = FlutterMethodChannel(name: "com.carverauto.chaseapp/nodle",
+                                              binaryMessenger: controller.binaryMessenger)
+      nodleChannel.setMethodCallHandler({
+          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          switch call.method {
+            case "init":
+              nodle = Nodle.sharedInstance
+            case "start":
+              self?.startNodle(result: result)
+          default:
+              result(FlutterMethodNotImplemented)
+          }
+      })
     GeneratedPluginRegistrant.register(with: self)
-    let nodle = Nodle.sharedInstance
-    Nodle().start("ss58:5CYDxNUNrRJU3s6fb1VPhNpNPwyTcFLQuTzmJg5mioBe2eN1");
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func startNodle(result: FlutterResult) {
+        nodle?.start(devKey: "ss58:5CYDxNUNrRJU3s6fb1VPhNpNPwyTcFLQuTzmJg5mioBe2eN1", tags: "", "")
+    }
+
 }
+
