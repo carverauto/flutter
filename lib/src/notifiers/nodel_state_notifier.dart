ea94ddef472dc3a712c4be43b684f8734d93c871
rutvik110 <takrutvik@gmail.com>
2022-01-20 08:46:47 +0530

    getter added for recieving value

--- lib/src/notifiers/nodel_state_notifier.dart
+++ lib/src/notifiers/nodel_state_notifier.dart
@@ -8,13 +8,13 @@ class NodelStateNotifier extends StateNotifier<NodleState> {
   NodelStateNotifier() : super(NodleState.Idle);
 
   static const platform = MethodChannel('com.carverauto.chaseapp/nodle');
-  late final String value;
-  // TODO: Can be initialized in the startup services
-  // Define an async function to initialize Nodle SDK
+  late final String _value;
+  String get value => _value;
+
   void initializeNodle() async {
     try {
-      value = await platform.invokeMethod("init");
-      log("Nodle initialized: $value");
+      _value = await platform.invokeMethod("init");
+      log("Nodle initialized: $_value");
     } catch (e) {
       log("Error Initializing Nodle", error: e);
     }
