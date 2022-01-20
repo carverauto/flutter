69000dfc24559e8b8d083cfa4f225ce8f7e9ad28
rutvik110 <takrutvik@gmail.com>
2022-01-20 08:03:18 +0530

    moved handling of firebase notifications & dynamic links to HomeWrapper

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:permission_handler/permission_handler.dart';
 
+//TODO: Should go into root
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
@@ -23,10 +25,15 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print("Handling a background message: ${message.messageId}");
 }
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.grey[400],
+      statusBarIconBrightness: Brightness.light));
   await Firebase.initializeApp();
-
+  //TODO: Should go in the root
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   // Setting up const/singletons like this will be redundant after refactoring
