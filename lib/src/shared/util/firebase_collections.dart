ec908b9bd02ce15cfd2c6ede254df55190febd7f
rutvik110 <takrutvik@gmail.com>
2022-01-20 18:50:49 +0530

    firebase top level providers and collectionsRef added

--- lib/src/shared/util/firebase_collections.dart
+++ lib/src/shared/util/firebase_collections.dart
@@ -1,3 +1,19 @@
 //TODO: Add all firebase collections here with convertors and refer this
 // declarations for using collections
 
+import 'package:chaseapp/src/models/user/user_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = FirebaseFirestore.instance;
+
+final CollectionReference<UserData> usersCollectionRef =
+    _firestore.collection("users").withConverter<UserData>(
+  fromFirestore: (data, _) {
+    final rawData = data.data()!;
+
+    return UserData.fromJson(rawData);
+  },
+  toFirestore: (data, _) {
+    return data.toJson();
+  },
+);
--- lib/src/top_level_providers/firebase_providers.dart
+++ lib/src/top_level_providers/firebase_providers.dart
@@ -1,1 +1,32 @@
-//TODO: Singleton providers for Firebase Services
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final googleSignInProvider = Provider<GoogleSignIn>((ref) => GoogleSignIn());
+
+//Klaro
+// final functionsProvider = Provider<FirebaseFunctions>((ref) {
+//   return FirebaseFunctions.instanceFor(region: "us-central1");
+// });
+
+final storageProvider = Provider<FirebaseStorage>((ref) {
+  return FirebaseStorage.instance;
+});
+final firebaseMesssagingProvider = Provider<FirebaseMessaging>((ref) {
+  return FirebaseMessaging.instance;
+});
+
+final firestoreProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
+});
+// final firebaseRemoteConfigProvider =
+//     Provider<RemoteConfig>((ref) => RemoteConfig.instance);
+// final firebaseCrashlyticsProvider =
+//     Provider<FirebaseCrashlytics>((ref) => FirebaseCrashlytics.instance);
