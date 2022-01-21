10990af5f803637e6c62144fe8359f44dc84d6eb
rutvik110 <takrutvik@gmail.com>
2022-01-21 12:59:20 +0530

    minor refactoring

--- lib/src/modules/signin/view/providers/providers.dart
+++ lib/src/modules/signin/view/providers/providers.dart
@@ -1,4 +1,3 @@
-import 'package:chaseapp/src/modules/signin/view/state_notifiers/post_login_state_notifier.dart';
 import 'package:chaseapp/src/modules/signin/view/state_notifiers/signin_view_model_notifier.dart';
 import 'package:chaseapp/src/shared/enums/view_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,8 +5,3 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final signInProvider =
     StateNotifierProvider<SignInViewModelStateNotifier, ViewState>(
         (ref) => SignInViewModelStateNotifier());
-
-final postLoginStateNotifier =
-    StateNotifierProvider<PostLoginStateNotifier, AsyncValue<void>>((ref) {
-  return PostLoginStateNotifier(ref.read);
-});
