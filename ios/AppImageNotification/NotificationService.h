f7e7426b48a86824965dcc32f6cf1b9cab71a4c2
rutvik110 <takrutvik@gmail.com>
2022-01-22 18:08:56 +0530

    new target in Xcode created which is required for showing Images within notifications on ios

---
+++ ios/AppImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  AppImageNotification
+//
+//  Created by rutvik tak on 22/01/22.
+//
+#import "FirebaseMessaging.h"
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ios/AppImageNotification/NotificationService.m
@@ -0,0 +1,34 @@
+//
+//  NotificationService.m
+//  AppImageNotification
+//
+//  Created by rutvik tak on 22/01/22.
+//
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    [[FIRMessaging extensionHelper] populateNotificationContent:self.bestAttemptContent withContentHandler:contentHandler];
+
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
