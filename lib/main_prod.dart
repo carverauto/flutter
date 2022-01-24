a32b400db408755bb3d394a74906e9f7ff66c632
rutvik110 <takrutvik@gmail.com>
2022-01-24 14:32:16 +0530

    Flavors set up and configured for android atm

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,22 +1,15 @@
-import 'package:chaseapp/src/root.dart';
-import 'package:chaseapp/src/core/top_level_providers/nodle_provider.dart';
+import 'package:chaseapp/flavors.dart';
+import 'package:chaseapp/main_common.dart';
 import 'package:chaseapp/src/core/top_level_providers/services_providers.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:chaseapp/src/root.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.grey[400],
-      statusBarIconBrightness: Brightness.light));
-  await Firebase.initializeApp();
-
+  F.appFlavor = Flavor.PROD;
+  await setUpServices();
   // Setting up const/singletons like this will be redundant after refactoring
   // Prefs.init();
   // Initialize other services like SharedPreferances, etc and provide through providers
