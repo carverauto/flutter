cfa323a2a6c8ebf0c9bd5dd9fc93d3b81ff75b2b
rutvik110 <takrutvik@gmail.com>
2022-01-26 17:04:26 +0530

    constants updated

--- lib/src/const/assets.dart
+++ lib/src/const/assets.dart
@@ -1,1 +1,4 @@
 const String donutSVG = 'assets/donut2.svg';
+const String donutImage = "images/donut.png";
+
+const String chaseAppNameImage = "images/chaseapp.png";
---
+++ lib/src/const/links.dart
@@ -0,0 +1,4 @@
+const String privacyPolicy = 'https://chaseapp.tv/privacy';
+const String tosPolicy = 'https://chaseapp.tv/tos';
+
+const String defaultPhotoURL = 'https://chaseapp.tv/icon.png';
