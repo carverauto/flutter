b9523ccf22b08f916464277484ebbd91f58a1e14
rutvik110 <takrutvik@gmail.com>
2022-01-26 17:03:21 +0530

    sizing helpers, const values added

---
+++ lib/src/const/sizings.dart
@@ -0,0 +1,46 @@
+import 'package:chaseapp/src/shared/util/helpers/sizescaleconfig.dart';
+
+final double profileImageSize = Sizescaleconfig.scalehightfactor(100);
+
+double kVerticalSizeSmall = Sizescaleconfig.scalehightfactor(10);
+double kVerticalSizeMedium = Sizescaleconfig.scalehightfactor(17);
+double kVerticalSizeLarge = Sizescaleconfig.scalehightfactor(25);
+
+double kImageSizeSmall = Sizescaleconfig.scalehightfactor(20);
+double kImageSizeMedium = Sizescaleconfig.scalehightfactor(34);
+double kImageSizeLarge = Sizescaleconfig.scalehightfactor(75);
+
+const double kVerticalSizeSmallConstant = 10;
+const double kVerticalSizeMediumConstant = 17;
+const double kVerticalSizeLargeConstant = 25;
+
+final double kIconSizeSmall = Sizescaleconfig.scalehightfactor(18);
+const double kIconSizeSmallConstant = 18;
+const double kIconSizeMediumConstant = 35;
+const double kIconSizeLargeConstant = 47;
+const double kIconSizeExtraLargeConstant = 65;
+double kIconSizeExtraLarge = Sizescaleconfig.scalehightfactor(65);
+
+const double kPaddingSmallConstant = 10;
+const double kPaddingMediumConstant = 20;
+const double kPaddingLargeConstant = 30;
+
+const double kBorderRadiusSmallConstant = 8;
+const double kBorderRadiusMediumConstant = 20;
+const double kBorderRadiusLargeConstant = 30;
+
+const double kBorderSideWidthSmallConstant = 1;
+const double kBorderSideWidthMediumConstant = 3;
+const double kBorderSideWidthLargeConstant = 5;
+
+const double kButtonPaddingSmall = 5;
+const double kButtonPaddingMedium = 10;
+const double kButtonPaddingLarge = 15;
+
+const double kAppBarIconSize = 50;
+
+const double kElevation = 1;
+
+const double kItemsSpacingSmall = 10;
+const double kItemsSpacingMedium = 20;
+const double kItemsSpacingLarge = 30;
