d02647c9c90ec371200f2c2428400c51e16f8e45
rutvik110 <takrutvik@gmail.com>
2022-01-26 17:06:52 +0530

    setting up sizes

--- lib/src/modules/auth/view/pages/auth_view_wrapper.dart
+++ lib/src/modules/auth/view/pages/auth_view_wrapper.dart
@@ -2,14 +2,19 @@ import 'dart:developer';
 
 import 'package:chaseapp/src/modules/auth/view/pages/login_register.dart';
 import 'package:chaseapp/src/modules/auth/view/providers/providers.dart';
-import 'package:chaseapp/src/modules/home/view/pages/home_page.dart';
 import 'package:chaseapp/src/modules/home/view/pages/home_wrapper.dart';
+import 'package:chaseapp/src/shared/util/helpers/sizescaleconfig.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AuthViewWrapper extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Sizescaleconfig.setSizes(
+      MediaQuery.of(context).size.height,
+      MediaQuery.of(context).size.width,
+      MediaQuery.of(context).textScaleFactor,
+    );
     final loggedinstatus = ref.watch(streamLogInStatus);
 
     return loggedinstatus.when(
