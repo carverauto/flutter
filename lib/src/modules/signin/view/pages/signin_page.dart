cc8d1467d1292e71798f3782a3f6276d543250f4
rutvik110 <takrutvik@gmail.com>
2022-02-01 19:06:28 +0530

    added social logins icons with bottom tos and pri policy message

--- lib/src/modules/signin/view/pages/signin_page.dart
+++ lib/src/modules/signin/view/pages/signin_page.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
+import 'package:chaseapp/src/const/links.dart';
 import 'package:chaseapp/src/const/sizings.dart';
 import 'package:chaseapp/src/const/textstyles.dart';
 import 'package:chaseapp/src/models/login_state/login_state.dart';
 import 'package:chaseapp/src/modules/signin/view/parts/multi_auth_dialog.dart';
 import 'package:chaseapp/src/modules/signin/view/providers/providers.dart';
 import 'package:chaseapp/src/shared/enums/social_logins.dart';
+import 'package:chaseapp/src/shared/util/helpers/launchLink.dart';
 import 'package:chaseapp/src/shared/util/helpers/sizescaleconfig.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LogInView extends ConsumerWidget {
   LogInView({Key? key}) : super(key: key);
@@ -40,91 +44,146 @@ class LogInView extends ConsumerWidget {
           });
     });
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Consumer(builder: (context, ref, _) {
-              final state = ref.watch(signInProvider);
-              return state.maybeWhen(loading: () {
-                return const LinearProgressIndicator();
-              }, orElse: () {
-                return SizedBox.shrink();
-              });
-            }),
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(kPaddingMediumConstant),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ElevatedButton(
-                  style: callToActionButtonStyle,
-                  onPressed: () {
-                    ref
-                        .read(signInProvider.notifier)
-                        .signIn(SIGNINMETHOD.GOOGLE);
-                  },
-                  child: Text(
-                    "Continue With Google",
-                    style: getButtonStyle(context),
-                  ),
-                ),
-                SizedBox(
-                  height: kItemsSpacingSmall,
-                ),
-                if (Platform.isIOS)
-                  ElevatedButton(
+      body: Padding(
+        padding: const EdgeInsets.all(kPaddingMediumConstant),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Consumer(builder: (context, ref, _) {
+                final state = ref.watch(signInProvider);
+                return state.maybeWhen(loading: () {
+                  return const LinearProgressIndicator();
+                }, orElse: () {
+                  return SizedBox.shrink();
+                });
+              }),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(kPaddingMediumConstant),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ElevatedButton.icon(
+                    icon: SvgPicture.asset(
+                      "assets/icon/google.svg",
+                      height: kIconSizeLargeConstant,
+                    ),
                     style: callToActionButtonStyle,
                     onPressed: () {
                       ref
                           .read(signInProvider.notifier)
-                          .signIn(SIGNINMETHOD.APPLE);
+                          .signIn(SIGNINMETHOD.GOOGLE);
                     },
-                    child: Text(
-                      "Continue With Apple",
+                    label: Text(
+                      "Continue With Google",
                       style: getButtonStyle(context),
                     ),
                   ),
-                if (Platform.isIOS)
                   SizedBox(
                     height: kItemsSpacingSmall,
                   ),
-                ElevatedButton(
-                  style: callToActionButtonStyle,
-                  onPressed: () {
-                    ref
-                        .read(signInProvider.notifier)
-                        .signIn(SIGNINMETHOD.FACEBOOK);
-                  },
-                  child: Text(
-                    "Continue With Facebook",
-                    style: getButtonStyle(context),
+                  if (Platform.isIOS)
+                    ElevatedButton.icon(
+                      icon: SvgPicture.asset(
+                        "assets/icon/apple.svg",
+                        height: kIconSizeLargeConstant,
+                      ),
+                      style: callToActionButtonStyle,
+                      onPressed: () {
+                        ref
+                            .read(signInProvider.notifier)
+                            .signIn(SIGNINMETHOD.APPLE);
+                      },
+                      label: Text(
+                        "Continue With Apple",
+                        style: getButtonStyle(context),
+                      ),
+                    ),
+                  if (Platform.isIOS)
+                    SizedBox(
+                      height: kItemsSpacingSmall,
+                    ),
+                  ElevatedButton.icon(
+                    icon: SvgPicture.asset(
+                      "assets/icon/facebook.svg",
+                      height: kIconSizeLargeConstant,
+                    ),
+                    style: callToActionButtonStyle,
+                    onPressed: () {
+                      ref
+                          .read(signInProvider.notifier)
+                          .signIn(SIGNINMETHOD.FACEBOOK);
+                    },
+                    label: Text(
+                      "Continue With Facebook",
+                      style: getButtonStyle(context),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: kItemsSpacingSmall,
-                ),
-                ElevatedButton(
-                  style: callToActionButtonStyle,
-                  onPressed: () {
-                    ref
-                        .read(signInProvider.notifier)
-                        .signIn(SIGNINMETHOD.TWITTER);
-                  },
-                  child: Text(
-                    "Continue With Twitter",
-                    style: getButtonStyle(context),
+                  SizedBox(
+                    height: kItemsSpacingSmall,
                   ),
-                ),
-              ],
+                  ElevatedButton.icon(
+                    icon: SvgPicture.asset(
+                      "assets/icon/twitter.svg",
+                      height: kIconSizeLargeConstant,
+                    ),
+                    style: callToActionButtonStyle,
+                    onPressed: () {
+                      ref
+                          .read(signInProvider.notifier)
+                          .signIn(SIGNINMETHOD.TWITTER);
+                    },
+                    label: Text(
+                      "Continue With Twitter",
+                      style: getButtonStyle(context),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Spacer(),
-        ],
+            Spacer(),
+            RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(children: [
+                  TextSpan(
+                    text: "By signing in you agree to the",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primaryVariant,
+                    ),
+                  ),
+                  TextSpan(
+                    text: " terms of service",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        launchUrl(tosPolicy);
+                      },
+                  ),
+                  TextSpan(
+                    text: " and ",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primaryVariant,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "privacy policy",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        launchUrl(privacyPolicy);
+                      },
+                  ),
+                ]))
+          ],
+        ),
       ),
     );
   }
