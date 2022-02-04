68f39bb864603fc950ae2fc9af2751403bf9cd67
rutvik110 <takrutvik@gmail.com>
2022-02-04 13:34:59 +0530

    call to action button styles updated

--- lib/src/const/textstyles.dart
+++ lib/src/const/textstyles.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 
 TextStyle getButtonStyle(BuildContext context) =>
     Sizescaleconfig.getDeviceType == DeviceType.MOBILE
-        ? Theme.of(context).textTheme.subtitle1!.copyWith(
+        ? Theme.of(context).textTheme.subtitle2!.copyWith(
               color: Theme.of(context).colorScheme.onPrimary,
             )
-        : Theme.of(context).textTheme.headline5!.copyWith(
+        : Theme.of(context).textTheme.headline6!.copyWith(
               color: Theme.of(context).colorScheme.onPrimary,
             );
