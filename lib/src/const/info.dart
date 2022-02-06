be2f03f42cc74eae9a32f149de8ff0a21de72ee8
Michael Freeman <mfreeman451@gmail.com>
2022-02-06 13:06:26 -0600

    Cleaning up screens .. again

--- lib/src/const/info.dart
+++ lib/src/const/info.dart
@@ -1,8 +1,8 @@
 final locationUsageInfo =
-    """The ChaseApp app captures device location data at regular intervals when the app is in use. When the app is closed or not in use, location data may be captured in the background.\n""";
+    """Device location data is captured at regular intervals when the app is in use. When the app is closed or not in use, location data may be captured in the background.\n""";
 
 final bluetoothUsageInfo =
-    """The ChaseApp app captures bluetooth scan data at regular intervals when the app is in use. When the app is closed or not in use, bluetooth scan data may be captured in the background \n""";
+    """Bluetooth scan data is captured at regular intervals when the app is in use. When the app is closed or not in use, bluetooth scan data may be captured in the background \n""";
 
 final notificationsUsageInfo =
-    """For receiving notifications credits new chases and updates.""";
+    """Needed for receiving notifications about live police pursuits and other events.""";
