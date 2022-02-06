4cef1ab8fddf1da7de7f981bf80e72aa8bc81caf
rutvik110 <takrutvik@gmail.com>
2022-02-06 11:23:37 +0530

    ui updates on permissions request

---
+++ lib/src/const/info.dart
@@ -0,0 +1,7 @@
+final locationUsageInfo =
+    """The ChaseApp captures device location data at regular intervals when the app is in use.\nUsers earn Nodle Cash based on the quality of data that is uploaded.\n""";
+
+final bluetoothUsageInfo =
+    """The ChaseApp captures bluetooth scan data at regular intervals when the app is in use.\nUsers earn Nodle Cash based on the quality of data that is uploaded.\n""";
+final notificationsUsageInfo =
+    """For recieving notifications about new chases and updates.""";
