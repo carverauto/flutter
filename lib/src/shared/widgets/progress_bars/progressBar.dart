ce0fe8bc50cc491beb18b5b5fa239e1708149ab9
rutvik110 <takrutvik@gmail.com>
2022-02-10 09:07:18 +0530

    dead code commented

--- lib/src/shared/widgets/progress_bars/progressBar.dart
+++ lib/src/shared/widgets/progress_bars/progressBar.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_progress_hud/flutter_progress_hud.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 
-class Utils extends StatelessWidget {
-  const Utils({Key? key}) : super(key: key);
+// class Utils extends StatelessWidget {
+//   const Utils({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ProgressHUD(
-      child: Builder(
-          builder: (context) => Center(
-            child: Column(
-              children: <Widget>[
-                ElevatedButton(onPressed: () {
-                  final progress = ProgressHUD.of(context);
-                  progress?.show();
-                }, child: const Text('Please wait'))
-              ]
-            )
-          )
-      )
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ProgressHUD(
+//       child: Builder(
+//           builder: (context) => Center(
+//             child: Column(
+//               children: <Widget>[
+//                 ElevatedButton(onPressed: () {
+//                   final progress = ProgressHUD.of(context);
+//                   progress?.show();
+//                 }, child: const Text('Please wait'))
+//               ]
+//             )
+//           )
+//       )
+//     );
+//   }
+// }
