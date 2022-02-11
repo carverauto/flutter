69cd975774fcd2e7540501063bff22619c2160b4
rutvik110 <takrutvik@gmail.com>
2022-02-11 16:08:09 +0530

    Added optional onTap callback to GlassButton

--- lib/src/shared/widgets/buttons/glass_button.dart
+++ lib/src/shared/widgets/buttons/glass_button.dart
@@ -9,34 +9,36 @@ class GlassButton extends StatelessWidget {
     Key? key,
     required this.child,
     this.padding,
+    this.onTap,
   }) : super(key: key);
 
   final Widget child;
   final EdgeInsets? padding;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(4),
+    return Material(
       clipBehavior: Clip.hardEdge,
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: blurValue,
-          sigmaY: blurValue,
-        ),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: primaryColor.shade500.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(
-              4,
-            ),
+      borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+      child: InkWell(
+        onTap: onTap,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: blurValue,
+            sigmaY: blurValue,
           ),
-          child: Padding(
-            padding: padding ??
-                const EdgeInsets.all(
-                  kPaddingSmallConstant / 2,
-                ),
-            child: child,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: primaryColor.shade500.withOpacity(0.1),
+            ),
+            child: Padding(
+              padding: padding ??
+                  const EdgeInsets.all(
+                    kPaddingSmallConstant / 2,
+                  ),
+              child: child,
+            ),
           ),
         ),
       ),
