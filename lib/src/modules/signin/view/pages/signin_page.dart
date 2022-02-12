d8de8e7d8324c064dcb8601f49d10b248e17a8bb
rutvik110 <takrutvik@gmail.com>
2022-02-12 13:32:30 +0530

    added a gradient animation to social login provider button when selected

--- lib/src/modules/signin/view/pages/signin_page.dart
+++ lib/src/modules/signin/view/pages/signin_page.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chaseapp/src/const/links.dart';
 import 'package:chaseapp/src/const/sizings.dart';
 import 'package:chaseapp/src/const/textstyles.dart';
 import 'package:chaseapp/src/models/login_state/login_state.dart';
+import 'package:chaseapp/src/modules/signin/view/parts/button_scale_transition.dart';
+import 'package:chaseapp/src/modules/signin/view/parts/gradient_animation_container.dart';
 import 'package:chaseapp/src/modules/signin/view/parts/multi_auth_dialog.dart';
 import 'package:chaseapp/src/modules/signin/view/providers/providers.dart';
 import 'package:chaseapp/src/shared/enums/social_logins.dart';
@@ -13,14 +16,69 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 
-class LogInView extends ConsumerWidget {
-  LogInView({Key? key}) : super(key: key);
+class LogInView extends ConsumerStatefulWidget {
+  const LogInView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _LogInViewState();
+}
+
+class _LogInViewState extends ConsumerState<LogInView>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<double> rotationAnimation;
+
+  SIGNINMETHOD? signinmethod = null;
+
+  void signInWith(SIGNINMETHOD method) {
+    setState(() {
+      signinmethod = method;
+    });
+    ref.read(signInProvider.notifier).signIn(method);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat();
+
+    rotationAnimation = Tween<double>(begin: 0, end: pi * 2).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.decelerate,
+      ),
+    );
+
+    animationController.forward();
+
+    animationController.addListener(() {
+      if (animationController.isCompleted) {
+        animationController.repeat();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     ref.listen<LogInState>(signInProvider, (oldState, newState) {
       newState.when(
-          data: () {},
+          data: () {
+            setState(() {
+              signinmethod = null;
+            });
+          },
           multiAuth: (authProviders, credentials) async {
             final SIGNINMETHOD? knownAuthProvider =
                 await showDialog<SIGNINMETHOD?>(
@@ -33,10 +91,17 @@ class LogInView extends ConsumerWidget {
               ref
                   .read(signInProvider.notifier)
                   .handleMutliProviderSignIn(knownAuthProvider, credentials);
+            } else {
+              setState(() {
+                signinmethod = null;
+              });
             }
           },
           loading: () {},
           error: (e, stk) {
+            setState(() {
+              signinmethod = null;
+            });
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
               content: Text(e.message),
             ));
@@ -49,98 +114,99 @@ class LogInView extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Consumer(builder: (context, ref, _) {
-                final state = ref.watch(signInProvider);
-                return state.maybeWhen(loading: () {
-                  return const LinearProgressIndicator();
-                }, orElse: () {
-                  return SizedBox.shrink();
-                });
-              }),
-            ),
             Spacer(),
             Padding(
               padding: const EdgeInsets.all(kPaddingMediumConstant),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  ElevatedButton.icon(
-                    icon: SvgPicture.asset(
-                      SIGNINMETHOD.GOOGLE.getAssetIcon,
-                      height: kIconSizeLargeConstant,
-                    ),
-                    style: callToActionButtonStyle,
-                    onPressed: () {
-                      ref
-                          .read(signInProvider.notifier)
-                          .signIn(SIGNINMETHOD.GOOGLE);
-                    },
-                    label: Text(
-                      "Continue With Google",
-                      style: getButtonStyle(context),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ButtonScaleAnimationWidget(
+                    child: GradientAnimationChildBuilder(
+                      shouldAnimate: signinmethod == SIGNINMETHOD.GOOGLE,
+                      child: ElevatedButton.icon(
+                        icon: SvgPicture.asset(
+                          SIGNINMETHOD.GOOGLE.getAssetIcon,
+                          height: kIconSizeLargeConstant,
+                        ),
+                        style: callToActionButtonStyle,
+                        onPressed: () {
+                          signInWith(SIGNINMETHOD.GOOGLE);
+                        },
+                        label: Text(
+                          "Continue With Google",
+                          style: getButtonStyle(context),
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(
                     height: kItemsSpacingSmall,
                   ),
                   if (Platform.isIOS)
-                    ElevatedButton.icon(
-                      icon: SvgPicture.asset(
-                        SIGNINMETHOD.APPLE.getAssetIcon,
-                        height: kIconSizeLargeConstant,
+                    ButtonScaleAnimationWidget(
+                        child: GradientAnimationChildBuilder(
+                      shouldAnimate: signinmethod == SIGNINMETHOD.APPLE,
+                      child: ElevatedButton.icon(
+                        icon: SvgPicture.asset(
+                          SIGNINMETHOD.APPLE.getAssetIcon,
+                          height: kIconSizeLargeConstant,
+                        ),
+                        style: callToActionButtonStyle,
+                        onPressed: () {
+                          signInWith(SIGNINMETHOD.APPLE);
+                        },
+                        label: Text(
+                          "Continue With Apple",
+                          style: getButtonStyle(context),
+                        ),
                       ),
-                      style: callToActionButtonStyle,
-                      onPressed: () {
-                        ref
-                            .read(signInProvider.notifier)
-                            .signIn(SIGNINMETHOD.APPLE);
-                      },
-                      label: Text(
-                        "Continue With Apple",
-                        style: getButtonStyle(context),
-                      ),
-                    ),
+                    )),
                   if (Platform.isIOS)
                     SizedBox(
                       height: kItemsSpacingSmall,
                     ),
-                  ElevatedButton.icon(
-                    icon: SvgPicture.asset(
-                      SIGNINMETHOD.FACEBOOK.getAssetIcon,
-                      height: kIconSizeLargeConstant,
+                  ButtonScaleAnimationWidget(
+                      child: GradientAnimationChildBuilder(
+                    shouldAnimate: signinmethod == SIGNINMETHOD.FACEBOOK,
+                    child: ElevatedButton.icon(
+                      icon: SvgPicture.asset(
+                        SIGNINMETHOD.FACEBOOK.getAssetIcon,
+                        height: kIconSizeLargeConstant,
+                      ),
+                      style: callToActionButtonStyle,
+                      onPressed: () {
+                        signInWith(SIGNINMETHOD.FACEBOOK);
+                      },
+                      label: Text(
+                        "Continue With Facebook",
+                        style: getButtonStyle(context),
+                      ),
                     ),
-                    style: callToActionButtonStyle,
-                    onPressed: () {
-                      ref
-                          .read(signInProvider.notifier)
-                          .signIn(SIGNINMETHOD.FACEBOOK);
-                    },
-                    label: Text(
-                      "Continue With Facebook",
-                      style: getButtonStyle(context),
-                    ),
-                  ),
+                  )),
                   SizedBox(
                     height: kItemsSpacingSmall,
                   ),
-                  ElevatedButton.icon(
-                    icon: SvgPicture.asset(
-                      SIGNINMETHOD.TWITTER.getAssetIcon,
-                      height: kIconSizeLargeConstant,
+                  ButtonScaleAnimationWidget(
+                      child: GradientAnimationChildBuilder(
+                    shouldAnimate: signinmethod == SIGNINMETHOD.TWITTER,
+                    child: ElevatedButton.icon(
+                      icon: SvgPicture.asset(
+                        SIGNINMETHOD.TWITTER.getAssetIcon,
+                        height: kIconSizeLargeConstant,
+                      ),
+                      style: callToActionButtonStyle,
+                      onPressed: () {
+                        signInWith(SIGNINMETHOD.TWITTER);
+                      },
+                      label: Text(
+                        "Continue With Twitter",
+                        style: getButtonStyle(context),
+                      ),
                     ),
-                    style: callToActionButtonStyle,
-                    onPressed: () {
-                      ref
-                          .read(signInProvider.notifier)
-                          .signIn(SIGNINMETHOD.TWITTER);
-                    },
-                    label: Text(
-                      "Continue With Twitter",
-                      style: getButtonStyle(context),
-                    ),
-                  ),
+                  )),
                 ],
               ),
             ),
---
+++ lib/src/modules/signin/view/parts/button_scale_transition.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ButtonScaleAnimationWidget extends StatelessWidget {
+  const ButtonScaleAnimationWidget({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.7, end: 1),
+      duration: Duration(milliseconds: 300),
+      curve: Curves.decelerate,
+      child: child,
+      builder: (context, animation, child) {
+        return Transform.scale(
+          scale: animation,
+          child: child,
+        );
+      },
+    );
+  }
+}
