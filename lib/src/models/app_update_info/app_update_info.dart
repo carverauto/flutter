acd530eac390972673819b87fe14f1724a491e60
rutvik110 <takrutvik@gmail.com>
2022-02-12 15:58:26 +0530

    update mechanism updated to support for ios and android update versions independantly

--- lib/src/models/app_update_info/app_update_info.dart
+++ lib/src/models/app_update_info/app_update_info.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class AppUpdateInfo {
   AppUpdateInfo({
     required this.app_store,
@@ -7,11 +9,27 @@ class AppUpdateInfo {
     required this.play_store,
   });
 
-  bool get shouldUpdate => current_app_version != latest_app_version;
+  bool get shouldUpdate {
+    if (Platform.isAndroid) {
+      return current_app_version != latest_app_version.android;
+    } else {
+      return current_app_version != latest_app_version.ios;
+    }
+  }
+
   bool get forceUpdate => force_update;
   final String current_app_version;
-  final String latest_app_version;
+  final LatestAppVersion latest_app_version;
   final bool force_update;
   final String play_store;
   final String app_store;
+}
+
+class LatestAppVersion {
+  LatestAppVersion({
+    required this.android,
+    required this.ios,
+  });
+  final String ios;
+  final String android;
 }
