ad683278abf5f781982653d4c4fe9fffce54db85
rutvik110 <takrutvik@gmail.com>
2022-02-14 16:46:18 +0530

    chat ui layout improvements

--- lib/src/modules/chase_view/view/parts/show_chats_dialog.dart
+++ lib/src/modules/chase_view/view/parts/show_chats_dialog.dart
@@ -1,11 +1,20 @@
 import 'package:chaseapp/src/const/sizings.dart';
+import 'package:chaseapp/src/core/top_level_providers/services_providers.dart';
 import 'package:chaseapp/src/models/chase/chase.dart';
-import 'package:chaseapp/src/modules/chase_view/view/providers/providers.dart';
+import 'package:chaseapp/src/shared/widgets/errors/error_widget.dart';
+import 'package:chaseapp/src/shared/widgets/loaders/loading.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 void showChatsDialog(BuildContext context, Chase chase) {
+  final channel = client.channel(
+    'livestream',
+    id: chase.id,
+    extraData: {
+      'name': chase.name ?? "NA",
+    },
+  );
+  channel.watch();
   showModalBottomSheet<void>(
     context: context,
     // enableDrag: false,
@@ -23,81 +32,69 @@ void showChatsDialog(BuildContext context, Chase chase) {
         duration: Duration(milliseconds: 300),
         padding:
             EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.background,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(kBorderRadiusStandard),
-                    topRight: Radius.circular(kBorderRadiusStandard),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+              color: Theme.of(context).colorScheme.background,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(kPaddingMediumConstant)
+                      .copyWith(bottom: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Chats",
+                        style: Theme.of(context).textTheme.headline6!.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          Icons.close_rounded,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                child: Consumer(
-                  builder: (context, ref, child) {
-                    final state = ref.watch(chatChannelProvider(chase));
-                    state.watch();
-                    return StreamChannel(
-                      channel: state,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.all(kPaddingMediumConstant)
-                                      .copyWith(bottom: 0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Chats",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline6!
-                                        .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onBackground,
-                                        ),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    icon: Icon(
-                                      Icons.close_rounded,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Divider(),
-                            SizedBox(
-                              height: MediaQuery.of(context).size.height * 0.4,
-                              child: MessageListView(
-                                keyboardDismissBehavior:
-                                    ScrollViewKeyboardDismissBehavior.manual,
-                              ),
-                            ),
-                            MessageInput(
-                              disableAttachments: true,
-                            ),
-                          ],
+                Divider(),
+                Expanded(
+                  child: StreamChannel(
+                    channel: channel,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: MessageListView(
+                            loadingBuilder: (context) =>
+                                CircularAdaptiveProgressIndicatorWithBg(),
+                            errorBuilder: (context, e) {
+                              return ChaseAppErrorWidget(onRefresh: () {
+                                // ref.refresh(chatChannelProvider(chase));
+                              });
+                            },
+                            keyboardDismissBehavior:
+                                ScrollViewKeyboardDismissBehavior.manual,
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                        MessageInput(
+                          disableAttachments: true,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
