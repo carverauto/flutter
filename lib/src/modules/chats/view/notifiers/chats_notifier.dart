8c50c9b4f3abab649c170896623e6e9c02e7ea3e
rutvik110 <takrutvik@gmail.com>
2022-02-16 11:06:47 +0530

    added chaseapp app key from stream

--- lib/src/modules/chats/view/notifiers/chats_notifier.dart
+++ lib/src/modules/chats/view/notifiers/chats_notifier.dart
@@ -1,28 +1,30 @@
 import 'package:chaseapp/src/const/links.dart';
 import 'package:chaseapp/src/models/user/user_data.dart';
+import 'package:chaseapp/src/modules/chats/view/providers/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart' as stream;
-import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 class ChatStateNotifier extends StateNotifier<void> {
   ChatStateNotifier(this.read) : super(null);
 
   final Reader read;
 
-  final Logger logger = stream.Logger("ChatsServiceStateNotifier");
+  final Logger logger = Logger("ChatsServiceStateNotifier");
 
-  final _client = StreamChatClient(
-    "2cfcx4vhp35x",
+  final _client = stream.StreamChatClient(
+    "uq7mwraum8nu",
     logLevel: Level.INFO,
   );
 
-  StreamChatClient get client => _client;
+  stream.StreamChatClient get client => _client;
 
   Future<void> connectUserToGetStream(UserData userData) async {
     try {
       //TODO:generate token from server
-      final userToken = await client.devToken(userData.uid).rawValue;
-      // final userToken =  await read(chatsRepoProvider).getUserToken(userData.uid);
+      //  final userToken = await client.devToken(userData.uid).rawValue;
+      final userToken =
+          await read(chatsRepoProvider).getUserToken(userData.uid);
 
       //TODO: Need to discuss?
       //Shouldn't this check be done within connectUser function instead?
