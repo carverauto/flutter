f6a9547e4538cfc0f0008d742b21b0a70d446ee4
rutvik110 <takrutvik@gmail.com>
2022-02-23 16:50:19 +0530

    notificationCollectionRef added

--- lib/src/shared/util/firebase_collections.dart
+++ lib/src/shared/util/firebase_collections.dart
@@ -1,9 +1,8 @@
 //TODO: Add all firebase collections here with convertors and refer this
 // declarations for using collections
 
-import 'dart:developer';
-
 import 'package:chaseapp/src/models/chase/chase.dart';
+import 'package:chaseapp/src/models/notification_data/notification_data.dart';
 import 'package:chaseapp/src/models/user/user_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -11,6 +10,7 @@ final _firestore = FirebaseFirestore.instance;
 
 final CollectionReference chasesCollection = _firestore.collection('chases');
 final CollectionReference usersCollection = _firestore.collection('users');
+// final CollectionReference notificationsCollection = _firestore.collection('users');
 
 final CollectionReference<UserData> usersCollectionRef =
     usersCollection.withConverter<UserData>(
@@ -36,3 +36,24 @@ final CollectionReference<Chase> chasesCollectionRef =
     return data.toJson();
   },
 );
+
+CollectionReference<NotificationData> notificationsCollectionRef(
+    String userId) {
+  final notificationsCollection = _firestore
+      .collection('users')
+      .doc(userId)
+      .collection('notifications')
+      .withConverter<NotificationData>(
+    fromFirestore: (data, _) {
+      final rawData = data.data()!;
+      rawData["id"] = data.id;
+
+      return NotificationData.fromJson(rawData);
+    },
+    toFirestore: (data, _) {
+      return data.toJson();
+    },
+  );
+
+  return notificationsCollection;
+}
