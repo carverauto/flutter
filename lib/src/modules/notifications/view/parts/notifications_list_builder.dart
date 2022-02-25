2fc477e0cc26dea550209d659c5dd70dd0b878e1
rutvik110 <takrutvik@gmail.com>
2022-02-25 10:46:59 +0530

    updates due to model change

--- lib/src/modules/notifications/view/parts/notifications_list_builder.dart
+++ lib/src/modules/notifications/view/parts/notifications_list_builder.dart
@@ -1,17 +1,8 @@
-import 'package:chaseapp/src/const/colors.dart';
-import 'package:chaseapp/src/const/links.dart';
-import 'package:chaseapp/src/const/sizings.dart';
 import 'package:chaseapp/src/models/notification_data/notification_data.dart';
 import 'package:chaseapp/src/models/pagination_state/pagination_notifier_state.dart';
-import 'package:chaseapp/src/modules/notifications/view/parts/notification_dialog.dart';
+import 'package:chaseapp/src/modules/notifications/view/parts/notification_tile.dart';
 import 'package:chaseapp/src/notifiers/pagination_notifier.dart';
-import 'package:chaseapp/src/routes/routeNames.dart';
-import 'package:chaseapp/src/shared/util/extensions/interest_enum.dart';
-import 'package:chaseapp/src/shared/util/helpers/date_added.dart';
-import 'package:chaseapp/src/shared/util/helpers/image_url_parser.dart';
 import 'package:chaseapp/src/shared/widgets/builders/SliverProviderPaginatedStateNotifierBuilder.dart';
-import 'package:chaseapp/src/shared/widgets/builders/image_builder.dart';
-import 'package:chaseapp/src/shared/widgets/hero_dialog_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
@@ -57,94 +48,8 @@ class NotificationsPaginatedListView extends ConsumerWidget {
               : SliverList(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
-                      return ListTile(
-                        onTap: () {
-                          switch (
-                              notifications[index].getInterestEnumFromName) {
-                            case Interests.chasesnotifications:
-                              Navigator.pushNamed(
-                                context,
-                                RouteName.CHASE_VIEW,
-                                arguments: <String, dynamic>{
-                                  "chaseId": notifications[index].data?["id"],
-                                },
-                              );
-                              break;
-                            default:
-                              Navigator.push(
-                                context,
-                                HeroDialogRoute<void>(
-                                  builder: (context) {
-                                    return NotificationDialog(
-                                      notificationData: notifications[index],
-                                    );
-                                  },
-                                ),
-                              );
-                          }
-                        },
-                        leading: AspectRatio(
-                          aspectRatio: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(2),
-                            child: Hero(
-                              tag: notifications[index].id,
-                              child: DecoratedBox(
-                                decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: primaryShadowColor,
-                                      blurRadius: blurValue,
-                                      offset: Offset(0, 2),
-                                    )
-                                  ],
-                                  color: Theme.of(context).cardColor,
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(4),
-                                  clipBehavior: Clip.hardEdge,
-                                  child: AdaptiveImageBuilder(
-                                    url: parseImageUrl(
-                                      notifications[index].image ??
-                                          defaultPhotoURL,
-                                      ImageDimensions.SMALL,
-                                    ),
-                                    //  notifications[index].image ??
-                                    //     defaultPhotoURL,
-                                    showLoading: false,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        title: Text(notifications[index].title ?? "NA",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onBackground,
-                            )),
-                        subtitle: Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                notifications[index].body ?? "NA",
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  color: primaryColor.shade300,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: kItemsSpacingExtraSmallConstant,
-                            ),
-                            Text(
-                              elapsedTimeForDate(
-                                  notifications[index].createdAt),
-                              style: TextStyle(
-                                color: primaryColor.shade300,
-                              ),
-                            ),
-                          ],
-                        ),
+                      return NotificationTIle(
+                        notificationData: notifications[index],
                       );
                     },
                     childCount: notifications.length,
