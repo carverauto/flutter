42cdc5b697de351bc36df43ce346883bd4e722b7
rutvik110 <takrutvik@gmail.com>
2022-03-08 22:17:45 +0530

    added a separate providerbuilder for chasedetails

--- lib/src/shared/widgets/builders/providerStateBuilder.dart
+++ lib/src/shared/widgets/builders/providerStateBuilder.dart
@@ -4,6 +4,85 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
 
+class ChaseDetailsProviderStateBuilder<T> extends ConsumerWidget {
+  const ChaseDetailsProviderStateBuilder({
+    Key? key,
+    required this.builder,
+    required this.watchThisProvider,
+    required this.logger,
+    this.errorMessage,
+    this.errorBuilder,
+    this.showBackButton = false,
+    this.loadingBuilder,
+    required this.chatsRow,
+    required this.chatsView,
+  }) : super(key: key);
+
+  final ProviderBase<AsyncValue<T>> watchThisProvider;
+
+  final Widget Function(
+      T data, WidgetRef ref, Widget chatsRow, Widget chatsView) builder;
+  final Widget Function(Object e, StackTrace? stk)? errorBuilder;
+
+  final Logger logger;
+
+  final String? errorMessage;
+  final bool showBackButton;
+  final Widget Function()? loadingBuilder;
+  final Widget chatsRow;
+  final Widget chatsView;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(watchThisProvider).when(
+        data: (data) {
+          return builder(
+            data,
+            ref,
+            chatsRow,
+            chatsView,
+          );
+        },
+        error: (e, stk) {
+          logger.severe(
+            errorMessage ?? 'Error Loading Data',
+            e,
+            stk,
+          );
+          return errorBuilder != null
+              ? errorBuilder!(e, stk)
+              : Material(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ChaseAppErrorWidget(
+                          onRefresh: () {
+                            ref.refresh(watchThisProvider);
+                          },
+                        ),
+                        if (showBackButton)
+                          ElevatedButton.icon(
+                            icon: Icon(Icons.arrow_back),
+                            label: Text('Back'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+        },
+        loading: () => loadingBuilder != null
+            ? loadingBuilder!()
+            : Material(
+                child: CircularAdaptiveProgressIndicatorWithBg(),
+              ));
+  }
+}
+
 class ProviderStateBuilder<T> extends ConsumerWidget {
   const ProviderStateBuilder({
     Key? key,
