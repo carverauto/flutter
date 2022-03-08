2e59573379dbb91e33a25dc1dfad402bb6322c36
rutvik110 <takrutvik@gmail.com>
2022-03-08 16:00:31 +0530

    firehose notification tile update

--- lib/src/modules/firehose/view/parts/firehose_notification_tile.dart
+++ lib/src/modules/firehose/view/parts/firehose_notification_tile.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:chaseapp/src/const/colors.dart';
 import 'package:chaseapp/src/const/sizings.dart';
 import 'package:chaseapp/src/models/notification/notification.dart';
 import 'package:chaseapp/src/models/tweet_data/tweet_data.dart';
@@ -7,9 +8,10 @@ import 'package:chaseapp/src/modules/firehose/view/parts/show_preview_dialog.dar
 import 'package:chaseapp/src/modules/firehose/view/providers/providers.dart';
 import 'package:chaseapp/src/shared/widgets/builders/providerStateBuilder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-class FirehoseNotificationTile extends StatelessWidget {
+class FirehoseNotificationTile extends ConsumerWidget {
   FirehoseNotificationTile({
     Key? key,
     required this.notification,
@@ -20,38 +22,26 @@ class FirehoseNotificationTile extends StatelessWidget {
   final Logger logger = Logger('FirehoseNotificationTile');
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     if (notification.title == "twitter") {
       return ProviderStateBuilder<TweetData>(
+          loadingBuilder: () => LoadingListTile(),
+          errorBuilder: (e, stk) {
+            return FirehoseErrorTile(
+                notification: notification,
+                onRefesh: () {
+                  ref.refresh(
+                      fetchTweetAlongUserData(notification.data!.tweetId!));
+                });
+          },
           builder: (tweetData, ref) {
-            return Padding(
-              padding: const EdgeInsets.only(
-                bottom: kItemsSpacingSmallConstant,
-              ),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(kBorderRadiusStandard),
-                ),
-                tileColor: Colors.white.withOpacity(0.5),
-                onTap: () {
-                  showFirehosePreview(notification, context);
-                },
-                leading: CircleAvatar(
-                  backgroundImage:
-                      CachedNetworkImageProvider(tweetData.profileImageUrl),
-                ),
-                title: Text(
-                  tweetData.text,
-                  maxLines: 2,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: ImageIcon(
-                  CachedNetworkImageProvider(notification.data!.image!),
-                  color: Colors.blue,
-                ),
+            return _FirehoseNotificationListTile(
+              notification: notification,
+              body: tweetData.text,
+              imageUrl: tweetData.profileImageUrl,
+              trailing: ImageIcon(
+                CachedNetworkImageProvider(notification.data!.image!),
+                color: Colors.blue,
               ),
             );
           },
@@ -59,39 +49,14 @@ class FirehoseNotificationTile extends StatelessWidget {
               fetchTweetAlongUserData(notification.data!.tweetId!),
           logger: logger);
     } else if (notification.title == "streams") {
-      return Padding(
-        padding: const EdgeInsets.only(
-          bottom: kItemsSpacingSmallConstant,
-        ),
-        child: ListTile(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(kBorderRadiusStandard),
-          ),
-          onTap: () {
-            showFirehosePreview(notification, context);
-          },
-          tileColor: Colors.white.withOpacity(0.5),
-          leading: CircleAvatar(
-            backgroundImage:
-                CachedNetworkImageProvider(notification.data!.image!),
-          ),
-          title: Text(
-            "Youtube",
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
-          ),
-          subtitle: Text(
-            notification.body!,
-            style: TextStyle(
-              color: primaryColor.shade300,
-            ),
-          ),
-          trailing: Icon(
-            Icons.play_arrow_rounded,
-            color: Colors.red,
-            size: kIconSizeMediumConstant,
-          ),
+      return _FirehoseNotificationListTile(
+        notification: notification,
+        body: notification.body,
+        imageUrl: notification.data!.image!,
+        trailing: Icon(
+          Icons.play_arrow_rounded,
+          color: Colors.red,
+          size: kIconSizeMediumConstant,
         ),
       );
     } else {
@@ -99,5 +64,140 @@ class FirehoseNotificationTile extends StatelessWidget {
         color: Colors.green,
       );
     }
+  }
+}
+
+class FirehoseErrorTile extends StatelessWidget {
+  const FirehoseErrorTile({
+    Key? key,
+    required this.notification,
+    required this.onRefesh,
+  }) : super(key: key);
+
+  final ChaseAppNotification notification;
+  final VoidCallback onRefesh;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white70,
+      title: TextButton.icon(
+        onPressed: onRefesh,
+        icon: IconButton(
+          onPressed: onRefesh,
+          icon: Icon(
+            Icons.refresh,
+            color: Colors.white,
+          ),
+        ),
+        label: Text(
+          "Failed to load tweet!",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      trailing: ImageIcon(
+        CachedNetworkImageProvider(notification.data!.image!),
+        color: Colors.blue,
+      ),
+    );
+  }
+}
+
+class _FirehoseNotificationListTile extends StatelessWidget {
+  const _FirehoseNotificationListTile({
+    Key? key,
+    required this.notification,
+    required this.body,
+    required this.trailing,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final ChaseAppNotification notification;
+  final String body;
+  final Widget trailing;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+      ),
+      onTap: () {
+        showFirehosePreview(notification, context);
+      },
+      tileColor: Colors.white70,
+      leading: CircleAvatar(
+        backgroundImage: CachedNetworkImageProvider(imageUrl),
+      ),
+      title: Text(
+        body,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onBackground,
+        ),
+      ),
+      trailing: trailing,
+    );
+  }
+}
+
+class LoadingListTile extends StatefulWidget {
+  const LoadingListTile({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingListTile> createState() => _LoadingListTileState();
+}
+
+class _LoadingListTileState extends State<LoadingListTile>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3000),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (context, child) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                transform: GradientRotation(pi / 4),
+                colors: [
+                  Colors.transparent,
+                  Colors.white70,
+                  Colors.transparent,
+                ],
+                stops: [
+                  0.0,
+                  animationController.value,
+                  1.0,
+                ],
+              ),
+            ),
+            height: 50,
+          );
+        });
   }
 }
