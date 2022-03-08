e04641ba74d3c094d563cf901b63c5814a971df0
rutvik110 <takrutvik@gmail.com>
2022-03-08 15:58:59 +0530

    firehose added  as enum

--- lib/src/shared/util/extensions/interest_enum.dart
+++ lib/src/shared/util/extensions/interest_enum.dart
@@ -1,10 +1,12 @@
-import 'package:chaseapp/src/models/interest/interest.dart';
 import 'package:chaseapp/src/models/notification/notification.dart';
+import 'package:chaseapp/src/shared/enums/interest_enum.dart';
 
 Interests getInterestEnumFromString(String interest) {
   switch (interest) {
     case "chases-notifications":
       return Interests.chases;
+    case "firehose-notifications":
+      return Interests.firehose;
     case "AppUpdates":
       return Interests.appUpdates;
     default:
@@ -12,34 +14,22 @@ Interests getInterestEnumFromString(String interest) {
   }
 }
 
-enum Interests {
-  chases,
-  appUpdates,
-  other,
-}
-
-extension InterestEnum on Interest {
-  Interests get getInterestEnum {
-    switch (this.name) {
-      case "chases-notifications":
-        return Interests.chases;
-      case "AppUpdates":
-        return Interests.appUpdates;
-      default:
-        return Interests.other;
-    }
-  }
-}
-
 extension NotificationInterestEnum on ChaseAppNotification {
-  Interests get getInterestEnumFromName {
-    switch (this.interest) {
-      case "chases-notifications":
-        return Interests.chases;
-      case "AppUpdates":
-        return Interests.appUpdates;
-      default:
-        return Interests.other;
-    }
+  Interests get getInterestEnumFromName =>
+      getInterestEnumFromString(this.interest);
+}
+
+String getStringFromInterestEnum(Interests interest) {
+  switch (interest) {
+    case Interests.chases:
+      return "chases-notifications";
+    case Interests.firehose:
+      return "firehose-notifications";
+    case Interests.appUpdates:
+      return "AppUpdates";
+    case Interests.other:
+      return "other";
+    default:
+      return "other";
   }
 }
