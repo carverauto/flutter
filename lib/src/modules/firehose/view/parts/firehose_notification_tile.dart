68bd883353e5eb87239f4d432a937e1e579c6d08
rutvik110 <takrutvik@gmail.com>
2022-03-08 12:34:20 +0530

    firehose tile update

--- lib/src/modules/firehose/view/parts/firehose_notification_tile.dart
+++ lib/src/modules/firehose/view/parts/firehose_notification_tile.dart
@@ -1,4 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chaseapp/src/const/colors.dart';
+import 'package:chaseapp/src/const/sizings.dart';
 import 'package:chaseapp/src/models/notification/notification.dart';
 import 'package:chaseapp/src/models/tweet_data/tweet_data.dart';
 import 'package:chaseapp/src/modules/firehose/view/parts/show_preview_dialog.dart';
@@ -22,22 +24,34 @@ class FirehoseNotificationTile extends StatelessWidget {
     if (notification.title == "twitter") {
       return ProviderStateBuilder<TweetData>(
           builder: (tweetData, ref) {
-            return ListTile(
-              onTap: () {
-                showFirehosePreview(notification, context);
-              },
-              leading: CircleAvatar(
-                backgroundImage:
-                    CachedNetworkImageProvider(tweetData.profileImageUrl),
+            return Padding(
+              padding: const EdgeInsets.only(
+                bottom: kItemsSpacingSmallConstant,
               ),
-              title: Text(
-                tweetData.text,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              trailing: ImageIcon(
-                CachedNetworkImageProvider(notification.data!.image!),
-                color: Colors.blue,
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+                ),
+                tileColor: Colors.white,
+                onTap: () {
+                  showFirehosePreview(notification, context);
+                },
+                leading: CircleAvatar(
+                  backgroundImage:
+                      CachedNetworkImageProvider(tweetData.profileImageUrl),
+                ),
+                title: Text(
+                  tweetData.text,
+                  maxLines: 2,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: ImageIcon(
+                  CachedNetworkImageProvider(notification.data!.image!),
+                  color: Colors.blue,
+                ),
               ),
             );
           },
@@ -45,19 +59,39 @@ class FirehoseNotificationTile extends StatelessWidget {
               fetchTweetAlongUserData(notification.data!.tweetId!),
           logger: logger);
     } else if (notification.title == "streams") {
-      return ListTile(
-        onTap: () {
-          showFirehosePreview(notification, context);
-        },
-        leading: CircleAvatar(
-          backgroundImage:
-              CachedNetworkImageProvider(notification.data!.image!),
+      return Padding(
+        padding: const EdgeInsets.only(
+          bottom: kItemsSpacingSmallConstant,
         ),
-        title: Text("Youtube"),
-        subtitle: Text(notification.body!),
-        trailing: ImageIcon(
-          CachedNetworkImageProvider(notification.data!.image!),
-          color: Colors.blue,
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+          ),
+          onTap: () {
+            showFirehosePreview(notification, context);
+          },
+          tileColor: Colors.white,
+          leading: CircleAvatar(
+            backgroundImage:
+                CachedNetworkImageProvider(notification.data!.image!),
+          ),
+          title: Text(
+            "Youtube",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+          subtitle: Text(
+            notification.body!,
+            style: TextStyle(
+              color: primaryColor.shade300,
+            ),
+          ),
+          trailing: Icon(
+            Icons.play_arrow_rounded,
+            color: Colors.red,
+            size: kIconSizeMediumConstant,
+          ),
         ),
       );
     } else {
