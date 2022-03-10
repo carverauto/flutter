1e039d41833f9704db8cb52aaea8cab16cdefde7
rutvik110 <takrutvik@gmail.com>
2022-03-10 18:20:09 +0530

    added youtube channel data model

---
+++ lib/src/models/youtube_data/youtube_data.dart
@@ -0,0 +1,12 @@
+class YoutubeChannelData {
+  YoutubeChannelData({
+    required this.channelId,
+    required this.name,
+    required this.imageUrl,
+    required this.subcribersCount,
+  });
+  String channelId;
+  String name;
+  String imageUrl;
+  int subcribersCount;
+}
