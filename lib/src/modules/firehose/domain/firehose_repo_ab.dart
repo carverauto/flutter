86d3208d65f417d5739c3b14b71f223e860111d1
rutvik110 <takrutvik@gmail.com>
2022-03-10 18:19:18 +0530

    firehose modules api updated with proper api for  fetch functions

--- lib/src/modules/firehose/domain/firehose_repo_ab.dart
+++ lib/src/modules/firehose/domain/firehose_repo_ab.dart
@@ -1,8 +1,15 @@
 import 'package:chaseapp/src/models/notification/notification.dart';
+import 'package:chaseapp/src/models/tweet_data/tweet_data.dart';
+import 'package:chaseapp/src/models/youtube_data/youtube_data.dart';
+import 'package:chaseapp/src/modules/firehose/view/providers/providers.dart';
 
 abstract class FirehoseRepoAB {
   Stream<List<ChaseAppNotification>> streamFirehoseNotifications();
 
   Future<List<ChaseAppNotification>> fetchNotifications(
       ChaseAppNotification? notificationData);
+
+  Future<TweetData> fetchTweetData(String tweetId);
+  Future<String> fetchTweetEmbedHtml(EmbedTweetParam tweetId);
+  Future<YoutubeChannelData> fetchYoutubeChannelData(String channelId);
 }
