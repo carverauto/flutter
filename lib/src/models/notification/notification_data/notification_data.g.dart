d1dca5df4dd66c04299abbe761d7894779d626d1
rutvik110 <takrutvik@gmail.com>
2022-03-22 07:22:14 +0530

    notificationdata model update to handle tweetData

--- lib/src/models/notification/notification_data/notification_data.g.dart
+++ lib/src/models/notification/notification_data/notification_data.g.dart
@@ -10,18 +10,20 @@ _$_NotificationData _$$_NotificationDataFromJson(Map<String, dynamic> json) =>
     _$_NotificationData(
       id: json['Id'] as String?,
       image: json['Image'] as String?,
-      tweetId: json['Tweetid'] as String?,
       youtubeId: json['YoutubeId'] as String?,
       channelId: json['ChannelId'] as String?,
       configState: json['ConfigState'] as String?,
+      tweetData: json['tweetData'] == null
+          ? null
+          : TweetData.fromJson(json['tweetData'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$$_NotificationDataToJson(_$_NotificationData instance) =>
     <String, dynamic>{
       'Id': instance.id,
       'Image': instance.image,
-      'Tweetid': instance.tweetId,
       'YoutubeId': instance.youtubeId,
       'ChannelId': instance.channelId,
       'ConfigState': instance.configState,
+      'tweetData': instance.tweetData?.toJson(),
     };
