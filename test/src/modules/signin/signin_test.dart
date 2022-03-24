6cba80f92f3f2edeb9cbb87f8b2e97059bbc54ec
rutvik110 <takrutvik@gmail.com>
2022-03-24 18:45:19 +0530

    added tests for getSignInEnumfromString

---
+++ test/src/modules/signin/signin_test.dart
@@ -0,0 +1,35 @@
+import 'package:chaseapp/src/shared/enums/social_logins.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('MultiAuth SignIn Providers Enum Getter From String', () {
+    test('apple.com', () {
+      final SIGNINMETHOD signInProvider = getSignInProviderHelper('apple.com');
+      expect(signInProvider, SIGNINMETHOD.Apple);
+    });
+    test('google.com', () {
+      final SIGNINMETHOD signInProvider = getSignInProviderHelper('google.com');
+      expect(signInProvider, SIGNINMETHOD.Google);
+    });
+    test('facebook.com', () {
+      final SIGNINMETHOD signInProvider =
+          getSignInProviderHelper('facebook.com');
+      expect(signInProvider, SIGNINMETHOD.Facebook);
+    });
+    test('twitter.com', () {
+      final SIGNINMETHOD signInProvider =
+          getSignInProviderHelper('twitter.com');
+      expect(signInProvider, SIGNINMETHOD.Twitter);
+    });
+    test('emailLink', () {
+      final SIGNINMETHOD signInProvider = getSignInProviderHelper('emailLink');
+      expect(signInProvider, SIGNINMETHOD.Email);
+    });
+    test('Unkown Provider', () {
+      expect(
+        () => getSignInProviderHelper('Unkown Provider'),
+        throwsException,
+      );
+    });
+  });
+}
