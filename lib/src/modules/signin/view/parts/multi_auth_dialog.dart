1b4c91f7a1db0d680bf35901c7e9d0958deff1f9
rutvik110 <takrutvik@gmail.com>
2022-03-25 18:34:35 +0530

    added dialog for showing when we don't support the sign in provider that user's trying to sign in with when shown the multi-auth dialog.

--- lib/src/modules/signin/view/parts/multi_auth_dialog.dart
+++ lib/src/modules/signin/view/parts/multi_auth_dialog.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../const/sizings.dart';
 import '../../../../shared/enums/social_logins.dart';
@@ -46,16 +49,91 @@ class MultiAuthDialog extends StatelessWidget {
               children: existingProviders.map<Widget>((String provider) {
                 final SIGNINMETHOD knownAuthProvider =
                     getSignInProviderHelper(provider);
+
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 5),
                   child: ElevatedButton(
                     onPressed: () {
                       Navigator.pop(context, knownAuthProvider);
                     },
-                    child: Text(knownAuthProvider.name),
+                    child: knownAuthProvider == SIGNINMETHOD.Unknown
+                        ? Text(toBeginningOfSentenceCase(provider).toString())
+                        : Text(knownAuthProvider.name),
                   ),
                 );
               }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotSupportedSignInProviderDialog extends StatelessWidget {
+  const NotSupportedSignInProviderDialog({
+    Key? key,
+    required this.authProviders,
+  }) : super(key: key);
+
+  final List<String> authProviders;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<SIGNINMETHOD> availableSigninAuthProviders =
+        authProviders.map(getSignInProviderHelper).toList()
+          ..removeWhere(
+            (SIGNINMETHOD e) => e == SIGNINMETHOD.Unknown,
+          );
+    if (Platform.isAndroid &&
+        availableSigninAuthProviders.contains(SIGNINMETHOD.Apple)) {
+      availableSigninAuthProviders.remove(SIGNINMETHOD.Apple);
+    }
+
+    return Dialog(
+      insetPadding: const EdgeInsets.all(kPaddingMediumConstant),
+      child: Padding(
+        padding: const EdgeInsets.all(kPaddingSmallConstant),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.info,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            Text(
+              "Looks like we don't support the sign in method you're trying to sign in with.\nPlease try one of the other methods to continue.",
+              style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: kItemsSpacingSmall,
+            ),
+            Builder(
+              builder: (BuildContext context) {
+                // final List<SIGNINMETHOD> fallBackProvidersList =
+                //     Platform.isAndroid
+                //         ? [SIGNINMETHOD.Google]
+                //         : [SIGNINMETHOD.Google, SIGNINMETHOD.Apple];
+
+                return Wrap(
+                  children: availableSigninAuthProviders
+                      .map<Widget>((SIGNINMETHOD provider) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context, provider);
+                        },
+                        child: Text(provider.name),
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ],
         ),
