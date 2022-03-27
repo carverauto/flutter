e46857a0ed63ae418ec83af65c630cff44f8569e
rutvik110 <takrutvik@gmail.com>
2022-03-27 13:06:41 +0530

    integration tests added

--- integration_test/src/modules/pusher_beams/pusher_beams_interests_test.dart
+++ integration_test/src/modules/pusher_beams/pusher_beams_interests_test.dart
@@ -1,14 +1,49 @@
-import 'package:chaseapp/main_common.dart';
+import 'package:chaseapp/main_dev.dart' as dev;
+import 'package:flutter_driver/flutter_driver.dart' as fdriver;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  final IntegrationTestWidgetsFlutterBinding binding =
+      IntegrationTestWidgetsFlutterBinding.ensureInitialized()
+          as IntegrationTestWidgetsFlutterBinding;
+
+  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+  final fdriver.FlutterDriver driver = await fdriver.FlutterDriver.connect();
+
+  // final String data = await driver.requestData(EnvVaribales.devInstanceId);
+
+  // print('PUsher key$data');
 
   testWidgets('failing test example', (WidgetTester tester) async {
     // expect(2 + 2, equals(5));
-    await tester.pumpWidget(const MyApp());
+    // Setting up const/singletons like this will be redundant after refactoring
+    // Prefs.init();
+    // Initialize other services like SharedPreferances, etc and provide through providers
 
+    dev.main();
+    await tester.pumpAndSettle();
+
+    // final PackageInfo info = await PackageInfo.fromPlatform();
+    // final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // F.appFlavor = Flavor.DEV;
+
+    // await setUpServices();
+
+    print('1');
+    // await tester.pumpWidget(
+    //   ProviderScope(
+    //     overrides: [
+    //       appInfoProvider.overrideWithValue(info),
+    //       sharedPreferancesProvider.overrideWithValue(prefs),
+    //     ],
+    //     observers: const [],
+    //     child: const MyApp(),
+    //   ),
+    // );
+    // print('2');
+    // await tester.pump(const Duration(seconds: 1));
+    // print('3');
     expect(1 + 1, 2);
   });
 }
