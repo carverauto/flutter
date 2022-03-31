49aecdfe6a7077012559574f10f39379b458bb30
rutvik110 <takrutvik@gmail.com>
2022-03-31 22:55:06 +0530

    chase events notifier added for handling event related business logic

---
+++ lib/src/modules/chase_view/view/notifiers/chase_events_notifier.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stream_feed_flutter_core/stream_feed_flutter_core.dart' as feed;
+
+import '../../../../models/chase/chase.dart';
+import '../../../../models/chase_animation_event.dart/chase_animation_event.dart';
+
+class ChaseEventsNotifier extends StateNotifier<AsyncValue<void>> {
+  ChaseEventsNotifier({
+    required this.chase,
+    required this.read,
+    required this.streamFeedClient,
+  }) : super(const AsyncValue.data(null));
+
+  final Reader read;
+
+  final Chase chase;
+
+  // late String videoId;
+
+  final feed.StreamFeedClient streamFeedClient;
+
+  final StreamController<ChaseAnimationEvent> streamController =
+      StreamController<ChaseAnimationEvent>();
+
+  // late List<ChaseAnimationEvent> _animationEvents;
+
+  feed.FlatFeed get firehoseFeed =>
+      streamFeedClient.flatFeed('events', chase.id);
+
+  Future<List<ChaseAnimationEvent>> fetchAnimationEvents() async {
+    // state = const AsyncValue.loading();
+    // //  final feed.Filter filter = feed.Filter().;
+    // try {
+    //   // final List<feed.Activity> activities = await firehoseFeed.getActivities(
+    //   //     // filter: filter,
+    //   //     // ranking:
+    //   //     );
+    //   await subscribeToEventsStream();
+    //   //convert to ChaseAnimationEvents objects
+    //   // state = ChaseAnimationEvents list;
+    //   state = AsyncValue.data(fakeAnimationEvents);
+    // } catch (e, stk) {
+    //   state = AsyncValue.error(e, stackTrace: stk);
+    // }
+    await Future<void>.delayed(const Duration(seconds: 5));
+    fakeAnimationEvents.sort(
+      (ChaseAnimationEvent a, ChaseAnimationEvent b) =>
+          a.label.compareTo(b.label),
+    );
+
+    return fakeAnimationEvents;
+  }
+
+  Future<void> subscribeToEventsStream() async {
+    if (chase.live ?? false) {
+      final feed.Subscription subscription = await firehoseFeed.subscribe(
+        (feed.RealtimeMessage<Object?, Object?, Object?, Object?>? message) {
+          //parsed chaseapp event
+          final ChaseAnimationEvent animationevent = ChaseAnimationEvent(
+            id: 'awdaw',
+            animtype: 'pop_up',
+            endpoint: 'assets/rive/rives_animated_emojis.riv',
+            animstate: '',
+            label: 10000,
+            videoId: 'videoId',
+            artboard: 'love',
+            animation: 'Animation 1',
+            createdAt: DateTime.now(),
+            alignment: Alignment.bottomRight,
+          );
+
+          streamController.add(animationevent);
+        },
+      );
+    }
+  }
+}
+
+final List<ChaseAnimationEvent> fakeAnimationEvents = List.generate(
+  10,
+  (int index) => ChaseAnimationEvent(
+    id: 'awdaw',
+    animtype: 'pop_up',
+    endpoint: 'assets/rive/rives_animated_emojis.riv',
+    animstate: '',
+    label: index * 5000,
+    videoId: 'videoId',
+    artboard: 'love',
+    animation: 'Animation 1',
+    createdAt: DateTime.now(),
+    alignment: Alignment.bottomRight,
+  ),
+);
--- lib/src/modules/chase_view/view/providers/providers.dart
+++ lib/src/modules/chase_view/view/providers/providers.dart
@@ -1,7 +1,44 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../models/chase/chase.dart';
+import '../../../chats/view/providers/providers.dart';
+import '../notifiers/chase_events_notifier.dart';
+
 // final chaseDetailsHeightProvider = StateProvider<double?>((ref) => null);
-final playVideoProvider = StateProvider.autoDispose<bool>((ref) => false);
-final showVideoOverlayProvider = StateProvider.autoDispose<bool>((ref) => true);
-final isShowingChatsWindowProvide =
-    StateProvider.autoDispose<bool>((ref) => false);
+final AutoDisposeStateProvider<bool> playVideoProvider =
+    StateProvider.autoDispose<bool>(
+  (AutoDisposeStateProviderRef<bool> ref) => false,
+);
+final AutoDisposeStateProvider<bool> showVideoOverlayProvider =
+    StateProvider.autoDispose<bool>(
+  (AutoDisposeStateProviderRef<bool> ref) => true,
+);
+final AutoDisposeStateProvider<bool> isShowingChatsWindowProvide =
+    StateProvider.autoDispose<bool>(
+  (AutoDisposeStateProviderRef<bool> ref) => false,
+);
+
+// final  StateNotifierProviderFamily<StateNotifierProvider<ChaseEventsNotifier, void>, List<ChaseAnimationEvent>, Chase> chasesEventsStateNotifierProvider = StateNotifierProvider.family<StateNotifierProvider<ChaseEventsNotifier, void> ,List<ChaseAnimationEvent>,Chase>((StateNotifierProvider<ChaseEventsNotifier, void> ref,Chase chase ) {
+//   return
+// });
+
+final StateNotifierProviderFamily<ChaseEventsNotifier, void, Chase>
+    chaseEventsNotifierProvider =
+    StateNotifierProvider.family<ChaseEventsNotifier, void, Chase>(
+  (StateNotifierProviderRef<ChaseEventsNotifier, void> ref, Chase chase) =>
+      ChaseEventsNotifier(
+    chase: chase,
+    read: ref.read,
+    streamFeedClient: ref.read(streamFeedClientProvider),
+  ),
+);
+
+// final FutureProviderFamily<List<ChaseAnimationEvent>, Chase>
+//     fetchAllChaseEvents =
+//     FutureProvider.family<List<ChaseAnimationEvent>, Chase>(
+//   (FutureProviderRef<List<ChaseAnimationEvent>> ref, Chase chase) async {
+//     return ref
+//         .read(chatsServiceStateNotifierProvider(chase).notifier)
+//         .fetchAnimationEvents();
+//   },
+// );
