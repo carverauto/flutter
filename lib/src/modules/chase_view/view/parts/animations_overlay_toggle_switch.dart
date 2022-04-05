513f9cdf68f3fa431bfb71591b2f7bcfb853899a
rutvik110 <takrutvik@gmail.com>
2022-04-05 13:44:25 +0530

    theme refactor

--- lib/src/modules/chase_view/view/parts/animations_overlay_toggle_switch.dart
+++ lib/src/modules/chase_view/view/parts/animations_overlay_toggle_switch.dart
@@ -1,6 +1,7 @@
-import 'package:chaseapp/src/modules/chase_view/view/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/providers.dart';
 
 class AnimationsOverlayToggleSwitch extends StatelessWidget {
   const AnimationsOverlayToggleSwitch({
@@ -10,17 +11,21 @@ class AnimationsOverlayToggleSwitch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Tooltip(
-      message: "Toggle Animations Overlay",
-      child: Consumer(builder: (context, ref, child) {
-        final bool showOverlay = ref.watch(showVideoOverlayProvider);
+      message: 'Toggle Animations Overlay',
+      child: Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          final bool showOverlay = ref.watch(showVideoOverlayProvider);
 
-        return Switch(
-          value: showOverlay,
-          onChanged: (value) {
-            ref.read(showVideoOverlayProvider.state).update((state) => value);
-          },
-        );
-      }),
+          return Switch(
+            value: showOverlay,
+            onChanged: (bool value) {
+              ref
+                  .read(showVideoOverlayProvider.state)
+                  .update((bool state) => value);
+            },
+          );
+        },
+      ),
     );
   }
 }
