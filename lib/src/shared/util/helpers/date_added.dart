c69160982ca721744bb6d908876b8ef1d13d3257
rutvik110 <takrutvik@gmail.com>
2022-04-26 12:40:08 +0530

    refactored date to string functions and added function to calculate chase duration in string

--- lib/src/shared/util/helpers/date_added.dart
+++ lib/src/shared/util/helpers/date_added.dart
@@ -1,65 +1,38 @@
-import 'package:chaseapp/src/models/chase/chase.dart';
 import 'package:intl/intl.dart';
 
+import '../../../models/chase/chase.dart';
+
 String dateAdded(Chase chase) {
-  late final String dateMsg;
   if (chase.createdAt == null) {
-    return "NA";
-  }
-  final chaseDate = DateTime.parse(chase.createdAt!.toIso8601String());
-  final today = DateTime.now().toLocal();
-  final diff = chaseDate.difference(today);
-  if (chase.live ?? false) {
-    dateMsg = 'LIVE!';
-  } else if (diff.inDays.abs() == 0) {
-    // Was the chase today?
-    if (diff.inHours.abs() == 0) {
-      // Chase in last hour?
-      if (diff.inMinutes == 0) {
-        // In the last minute? show seconds
-        dateMsg = diff.inSeconds.abs().toString() + ' seconds ago';
-      } else {
-        // Otherwise show how many minutes
-        dateMsg = diff.inMinutes.abs().toString() + ' minutes ago';
-      }
-    } else if (diff.inHours.abs() == 1) {
-      // One hour ago
-      dateMsg = diff.inHours.abs().toString() + ' hour ago';
-    } else if (diff.inHours.abs() > 1) {
-      // Hours ago
-      dateMsg = diff.inHours.abs().toString() + ' hours ago';
-    }
-  } else {
-    // More than a day ago, just print the date
-    dateMsg = chaseDate.toIso8601String().substring(0, 10);
+    return 'NA';
   }
 
-  return dateMsg;
+  return chase.live ?? false ? 'LIVE!' : elapsedTimeForDate(chase.createdAt!);
 }
 
 String elapsedTimeForDate(DateTime date) {
   late final String dateMsg;
 
-  final chaseDate = date;
-  final today = DateTime.now().toLocal();
-  final diff = chaseDate.difference(today);
+  final DateTime chaseDate = date;
+  final DateTime today = DateTime.now().toLocal();
+  final Duration diff = chaseDate.difference(today);
   if (diff.inDays.abs() == 0) {
     // Was the chase today?
     if (diff.inHours.abs() == 0) {
       // Chase in last hour?
       if (diff.inMinutes == 0) {
         // In the last minute? show seconds
-        dateMsg = diff.inSeconds.abs().toString() + ' seconds ago';
+        dateMsg = '${diff.inSeconds.abs()} seconds ago';
       } else {
         // Otherwise show how many minutes
-        dateMsg = diff.inMinutes.abs().toString() + ' minutes ago';
+        dateMsg = '${diff.inMinutes.abs()} minutes ago';
       }
     } else if (diff.inHours.abs() == 1) {
       // One hour ago
-      dateMsg = diff.inHours.abs().toString() + ' hr ago';
+      dateMsg = '${diff.inHours.abs()} hr ago';
     } else if (diff.inHours.abs() > 1) {
       // Hours ago
-      dateMsg = diff.inHours.abs().toString() + ' hrs ago';
+      dateMsg = '${diff.inHours.abs()} hrs ago';
     }
   } else if (today.year != chaseDate.year) {
     dateMsg = DateFormat('MMM d, yyyy').format(chaseDate);
@@ -69,4 +42,61 @@ String elapsedTimeForDate(DateTime date) {
   }
 
   return dateMsg;
+}
+
+String chaseDuration(DateTime? createdAt, DateTime? endedAt) {
+  if (createdAt == null || endedAt == null) {
+    return '';
+  } else {
+    final Duration diff = endedAt.difference(createdAt);
+
+    return _chaseDurationDiffString(diff);
+    // if (diff.inDays.abs() == 0) {
+    //   // Was the chase today?
+    //   if (diff.inHours.abs() == 0) {
+    //     // Chase in last hour?
+    //     if (diff.inMinutes == 0) {
+    //       // In the last minute? show seconds
+    //       dateMsg = '${diff.inSeconds.abs()} seconds ago';
+    //     } else {
+    //       // Otherwise show how many minutes
+    //       dateMsg = '${diff.inMinutes.abs()} minutes ago';
+    //     }
+    //   } else if (diff.inHours.abs() == 1) {
+    //     // One hour ago
+    //     dateMsg = '${diff.inHours.abs()} hr ago';
+    //   } else if (diff.inHours.abs() > 1) {
+    //     // Hours ago
+    //     dateMsg = '${diff.inHours.abs()} hrs ago';
+    //   }
+    // }
+
+  }
+}
+
+String _chaseDurationDiffString(Duration duration) {
+  // const Duration duration = Duration(
+  //   // days: 2,
+  //   hours: 4,
+  //   minutes: 64,
+  // );
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  final String twoDigitDays = twoDigits(duration.inDays);
+  final String twoDigitHrs = twoDigits(duration.inHours.remainder(24));
+  final String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+  // final String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+
+  final String daysString = twoDigitDays == '00'
+      ? ''
+      : "$twoDigitDays ${duration.inDays == 1 ? "day" : "days"}";
+  final String hrsString = twoDigitHrs == '00'
+      ? ''
+      : "$twoDigitHrs ${duration.inHours == 1 ? "hr" : "hrs"}";
+  final String minutesString = twoDigitMinutes == '00'
+      ? ''
+      : "$twoDigitMinutes ${duration.inMinutes == 1 ? "minute" : "minutes"}";
+
+  final String durationinString = '$daysString $hrsString $minutesString';
+
+  return durationinString;
 }
