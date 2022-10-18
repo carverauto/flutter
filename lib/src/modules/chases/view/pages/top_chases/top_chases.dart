55e031ed1477c6ff41c639d2848c0a020355611f
rutvik110 <takrutvik@gmail.com>
2022-10-18 10:17:17 +0530

    replaced carousalslider package with custom carousal slider implementation

--- lib/src/modules/chases/view/pages/top_chases/top_chases.dart
+++ lib/src/modules/chases/view/pages/top_chases/top_chases.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
@@ -7,6 +6,7 @@ import '../../../../../const/sizings.dart';
 import '../../../../../models/chase/chase.dart';
 import '../../../../../shared/util/helpers/sizescaleconfig.dart';
 import '../../../../../shared/widgets/builders/providerStateBuilder.dart';
+import '../../../../../shared/widgets/carousal_slider.dart';
 import '../../../../../shared/widgets/loaders/shimmer_tile.dart';
 import '../../providers/providers.dart';
 import 'top_chase_builder.dart';
@@ -51,60 +51,70 @@ class TopChasesListView extends ConsumerWidget {
                     ),
                   ],
                 )
-              : SizedBox(
-                  width: Sizescaleconfig.screenwidth,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      height: Sizescaleconfig.screenheight! * 0.4,
-                      enableInfiniteScroll: false,
-                      viewportFraction: 1,
-                    ),
-                    items: chases
-                        .asMap()
-                        .map<int, Widget>(
-                          (int index, Chase chase) => MapEntry(
-                            index,
-                            TweenAnimationBuilder<double>(
-                              key: UniqueKey(),
-                              duration: const Duration(milliseconds: 300),
-                              tween: Tween<double>(begin: 0.8, end: 1),
-                              builder: (BuildContext context, double value,
-                                  Widget? child) {
-                                return ScaleTransition(
-                                  scale: AlwaysStoppedAnimation(value),
-                                  child: child,
-                                );
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: kPaddingMediumConstant,
-                                ),
-                                child: Stack(
-                                  children: [
-                                    TopChaseBuilder(
-                                      chase: chase,
-                                    ),
-                                    Positioned(
-                                      right: kPaddingMediumConstant,
-                                      top: kPaddingMediumConstant,
-                                      child: Text(
-                                        '${index + 1} / ${chases.length}',
-                                        style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onBackground,
-                                        ),
+              : CarousalSlider(
+                  carouselOptions: CarouselOptions(
+                    height: Sizescaleconfig.screenheight! * 0.4,
+                  ),
+                  builder: (
+                    BuildContext context,
+                    int index,
+                    Widget child,
+                    double primaryAnimation,
+                    double secondaryAnimation,
+                  ) {
+                    return Transform.scale(
+                      scale: secondaryAnimation,
+                      child: child,
+                    );
+                  },
+                  childrens: chases
+                      .asMap()
+                      .map<int, Widget>(
+                        (int index, Chase chase) => MapEntry(
+                          index,
+                          TweenAnimationBuilder<double>(
+                            key: UniqueKey(),
+                            duration: const Duration(milliseconds: 300),
+                            tween: Tween<double>(begin: 0.8, end: 1),
+                            builder: (
+                              BuildContext context,
+                              double value,
+                              Widget? child,
+                            ) {
+                              return ScaleTransition(
+                                scale: AlwaysStoppedAnimation(value),
+                                child: child,
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: kPaddingMediumConstant,
+                              ),
+                              child: Stack(
+                                children: [
+                                  TopChaseBuilder(
+                                    chase: chase,
+                                  ),
+                                  Positioned(
+                                    right: kPaddingMediumConstant,
+                                    top: kPaddingMediumConstant,
+                                    child: Text(
+                                      '${index + 1} / ${chases.length}',
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onBackground,
                                       ),
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                        )
-                        .values
-                        .toList(),
-                  ),
+                        ),
+                      )
+                      .values
+                      .toList(),
                 ),
         );
       },
---
+++ lib/src/shared/widgets/carousal_slider.dart
@@ -0,0 +1,105 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class CarousalSlider extends StatefulWidget {
+  const CarousalSlider({
+    Key? key,
+    required this.childrens,
+    required this.builder,
+    required this.carouselOptions,
+  }) : super(key: key);
+
+  final List<Widget> childrens;
+  final CarouselOptions carouselOptions;
+  final Widget Function(
+    BuildContext context,
+    int index,
+    Widget child,
+    double primaryAnimation,
+    double secondaryAnimation,
+  ) builder;
+
+  @override
+  State<CarousalSlider> createState() => _CarousalSliderState();
+}
+
+class _CarousalSliderState extends State<CarousalSlider> {
+  late final PageController pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.carouselOptions.height,
+      child: PageView.builder(
+        controller: pageController,
+        itemCount: widget.childrens.length,
+        onPageChanged: (int value) {
+          log(value.toString());
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return AnimatedBuilder(
+            animation: pageController,
+            child: widget.childrens[index],
+            builder: (BuildContext context, Widget? child) {
+              final ScrollPosition position = pageController.position;
+              late final double value;
+              if (position.hasPixels && position.hasContentDimensions) {
+                final double? page = pageController.page;
+                if (page != null) {
+                  value = page - index;
+                }
+              } else {
+                final BuildContext storageContext =
+                    pageController.position.context.storageContext;
+                final double? previousSavedPosition =
+                    PageStorage.of(storageContext)?.readState(storageContext)
+                        as double?;
+                if (previousSavedPosition != null) {
+                  value = previousSavedPosition - index.toDouble();
+                } else {
+                  value = pageController.initialPage - index.toDouble();
+                }
+              }
+              final double distortionRatio =
+                  (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
+
+              final double distortionValue =
+                  Curves.linear.transform(distortionRatio);
+
+              return widget.builder(
+                context,
+                index,
+                child!,
+                value,
+                distortionValue,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CarouselOptions {
+  CarouselOptions({
+    required this.height,
+  });
+
+  final double height;
+}
