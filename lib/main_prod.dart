cf953cfc7f01ea9128d1ca7c9352d14f925ab94a
rutvik110 <takrutvik@gmail.com>
2022-11-10 13:07:16 +0530

    added runZoneGuard for catching errors happening within zones(like onpressed callbacks) which might not be handled by our firebase loggers

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -5,26 +8,30 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'flavors.dart';
 import 'main_common.dart';
-import 'main_dev.dart';
 import 'src/core/top_level_providers/services_providers.dart';
 // import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
-  F.appFlavor = Flavor.PROD;
-  await setUpServices();
-  // Setting up const/singletons like this will be redundant after refactoring
-  // Prefs.init();
-  // Initialize other services like SharedPreferances, etc and provide through providers
-  final PackageInfo info = await PackageInfo.fromPlatform();
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  runApp(
-    ProviderScope(
-      overrides: [
-        appInfoProvider.overrideWithValue(info),
-        sharedPreferancesProvider.overrideWithValue(prefs),
-      ],
-      observers: [ProvidersLogger()],
-      child: const MyApp(),
-    ),
+  await runZonedGuarded<Future<void>>(
+    () async {
+      F.appFlavor = Flavor.PROD;
+      await setUpServices();
+      // Setting up const/singletons like this will be redundant after refactoring
+      // Prefs.init();
+      // Initialize other services like SharedPreferances, etc and provide through providers
+      final PackageInfo info = await PackageInfo.fromPlatform();
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      runApp(
+        ProviderScope(
+          overrides: [
+            appInfoProvider.overrideWithValue(info),
+            sharedPreferancesProvider.overrideWithValue(prefs),
+          ],
+          observers: [ProvidersLogger()],
+          child: const MyApp(),
+        ),
+      );
+    },
+    FirebaseCrashlytics.instance.recordError,
   );
 }
