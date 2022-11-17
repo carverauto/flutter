de772c621b08150dd7e38bf8abc22509ea7783d9
rutvik110 <takrutvik@gmail.com>
2022-11-17 20:35:52 +0530

    updated chaseview to play an youtube or mp4 video based on networks tier and each individual streams tier

--- lib/src/modules/chase_view/view/parts/chase_hero.dart
+++ lib/src/modules/chase_view/view/parts/chase_hero.dart
@@ -1,11 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../const/images.dart';
+import '../../../../const/sizings.dart';
 import '../../../../models/chase/chase.dart';
 import '../../../../models/chase/network/chase_network.dart';
+import '../../../../models/chase/network/chase_stream/chase_stream.dart';
 import '../../../../shared/util/helpers/image_url_parser.dart';
 import '../../../../shared/util/helpers/is_valid_youtube_url.dart';
 import '../../../../shared/widgets/builders/image_builder.dart';
@@ -14,7 +15,7 @@ import '../providers/providers.dart';
 import 'mp4_player/mp4_player.dart';
 import 'watch_youtube_video_button.dart';
 
-class ChaseHeroSection extends ConsumerWidget {
+class ChaseHeroSection extends ConsumerStatefulWidget {
   const ChaseHeroSection({
     Key? key,
     required this.chaseId,
@@ -27,46 +28,100 @@ class ChaseHeroSection extends ConsumerWidget {
   final Widget youtubeVideo;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final List<ChaseNetwork>? networks =
-        ref.watch(chaseNetworksStateProvider(chaseId));
+  ConsumerState<ChaseHeroSection> createState() => _ChaseHeroSectionState();
+}
+
+class _ChaseHeroSectionState extends ConsumerState<ChaseHeroSection> {
+  String? youtubeUrl;
+  String? mp4Url;
+
+  void sortOutNetworksBasedOnTier() {
+    final List<ChaseNetwork>? chaseNetworks =
+        ref.read(chaseNetworksStateProvider(widget.chaseId));
     final List<ChaseNetwork> networksModifiable =
-        networks == null ? [] : [...networks];
+        chaseNetworks == null ? [] : [...chaseNetworks];
 
     // sort networks based on Tier value
-    if (networksModifiable != null && networksModifiable.length >= 2) {
+    if (networksModifiable == null || networksModifiable.isEmpty) {
+      return;
+    }
+    if (networksModifiable.length >= 2) {
       networksModifiable
           .sort((ChaseNetwork a, ChaseNetwork b) => a.tier!.compareTo(b.tier!));
     }
+    final List<ChaseStream> streams = List.empty(growable: true);
 
-    final bool? isLive = ref.watch(
-      chaseLiveStatusChaseProvider(chaseId),
-    );
-    final String? chaseImageUrl = ref.watch(
-      streamChaseProvider(chaseId)
-          .select((AsyncValue<Chase> value) => value.value?.imageURL),
-    );
-    const bool isYoutubeUrlPresent = false;
-    networksModifiable.any((ChaseNetwork network) {
-      final String? url = network.url;
+    for (final ChaseNetwork network in networksModifiable) {
+      streams
+        ..add(ChaseStream(tier: 0, url: network.url ?? ''))
+        ..add(ChaseStream(tier: 0, url: network.mp4Url ?? ''));
+      if (network.streams != null) {
+        final List<ChaseStream> sortedStreams = network.streams!;
+        if (sortedStreams.length >= 2) {
+          sortedStreams
+              .sort((ChaseStream a, ChaseStream b) => a.tier.compareTo(b.tier));
+        }
+
+        streams.addAll(sortedStreams);
+      }
+    }
+    if (streams.isEmpty) {
+      return;
+    }
+
+    youtubeUrl ??= streams.firstWhere((ChaseStream network) {
+      final String url = network.url;
 
       if (url != null) {
         return isValidYoutubeUrl(url);
       }
 
       return false;
-    });
-    String? mp4Url;
-    if (!isYoutubeUrlPresent) {
-      mp4Url =
-          'http://nbclim-download.edgesuite.net/Prod/NBCU_LM_VMS_-_KNBC/948/803/19486677468-1080pnbcstations.mp4';
-      networksModifiable
-          .firstWhereOrNull(
-            (ChaseNetwork network) =>
-                network.mp4Url != null && network.mp4Url!.isNotEmpty,
-          )
-          ?.mp4Url;
-    }
+    }).url;
+
+    mp4Url ??= streams.firstWhere((ChaseStream network) {
+      final String url = network.url;
+
+      if (url != null) {
+        return url.endsWith('.mp4');
+      }
+
+      return false;
+    }).url;
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    sortOutNetworksBasedOnTier();
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    ref.listen<List<ChaseNetwork>?>(
+      chaseNetworksStateProvider(widget.chaseId),
+      (
+        List<ChaseNetwork>? previous,
+        List<ChaseNetwork>? next,
+      ) {
+        sortOutNetworksBasedOnTier();
+      },
+    );
+
+    final bool? isLive = ref.watch(
+      chaseLiveStatusChaseProvider(widget.chaseId),
+    );
+    final String? chaseImageUrl = ref.watch(
+      streamChaseProvider(widget.chaseId)
+          .select((AsyncValue<Chase> value) => value.value?.imageURL),
+    );
+
+    final bool isYoutubeUrlPresent = youtubeUrl != null;
 
     return Stack(
       children: [
@@ -75,60 +130,87 @@ class ChaseHeroSection extends ConsumerWidget {
             builder: (BuildContext context, WidgetRef ref, Widget? child) {
               final bool isPlayVideo = ref.watch(playVideoProvider);
 
-              return isPlayVideo ? youtubeVideo : const SizedBox.shrink();
+              return isPlayVideo
+                  ? widget.youtubeVideo
+                  : const SizedBox.shrink();
             },
           ),
         if (!isYoutubeUrlPresent && mp4Url == null)
-          GestureDetector(
-            onTap: () {
-              if (isYoutubeUrlPresent) {
-                ref.read(playVideoProvider.state).update((bool state) => true);
-              }
-            },
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                ColoredBox(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: chaseImageUrl != null && chaseImageUrl.isNotEmpty
-                      ? CachedNetworkImage(
-                          fit: BoxFit.cover,
-                          maxWidthDiskCache: 750,
-                          maxHeightDiskCache: 421,
-                          memCacheHeight: 421,
-                          memCacheWidth: 750,
-                          imageUrl: parseImageUrl(
-                            imageURL!,
-                            ImageDimensions.LARGE,
-                          ),
-                          placeholder: (BuildContext context, String value) =>
-                              const CircularAdaptiveProgressIndicatorWithBg(),
-                          errorWidget: (
-                            BuildContext context,
-                            String value,
-                            dynamic value2,
-                          ) {
-                            return const ImageLoadErrorWidget();
-                          },
-                        )
-                      : const Image(
-                          fit: BoxFit.cover,
-                          image: AssetImage(defaultAssetChaseImage),
-                        ),
-                ),
-                if (isYoutubeUrlPresent)
-                  Align(
-                    child: RepaintBoundary(
-                      child: WatchYoutubeVideo(isLive: isLive ?? false),
+          Stack(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  if (isYoutubeUrlPresent) {
+                    ref
+                        .read(playVideoProvider.state)
+                        .update((bool state) => true);
+                  }
+                },
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    ColoredBox(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      child: chaseImageUrl != null && chaseImageUrl.isNotEmpty
+                          ? CachedNetworkImage(
+                              fit: BoxFit.cover,
+                              maxWidthDiskCache: 750,
+                              maxHeightDiskCache: 421,
+                              memCacheHeight: 421,
+                              memCacheWidth: 750,
+                              imageUrl: parseImageUrl(
+                                widget.imageURL!,
+                                ImageDimensions.LARGE,
+                              ),
+                              placeholder: (
+                                BuildContext context,
+                                String value,
+                              ) =>
+                                  const CircularAdaptiveProgressIndicatorWithBg(),
+                              errorWidget: (
+                                BuildContext context,
+                                String value,
+                                dynamic value2,
+                              ) {
+                                return const ImageLoadErrorWidget();
+                              },
+                            )
+                          : const Image(
+                              fit: BoxFit.cover,
+                              image: AssetImage(defaultAssetChaseImage),
+                            ),
                     ),
+                    if (isYoutubeUrlPresent)
+                      Align(
+                        child: RepaintBoundary(
+                          child: WatchYoutubeVideo(isLive: isLive ?? false),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: kPaddingSmallConstant,
+                top: kPaddingSmallConstant,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
                   ),
-              ],
-            ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
         // show the mp4 video
         if (!isYoutubeUrlPresent && mp4Url != null)
           Mp4VideoPlayerView(
-            mp4Url: mp4Url,
+            mp4Url: mp4Url!,
           ),
       ],
     );
