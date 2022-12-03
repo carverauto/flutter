2dc32ce7236b7b67f59e7b343aafaeac7eff71c4
rutvik110 <takrutvik@gmail.com>
2022-12-04 03:31:29 +0530

    using methodchannels to check for Local Network access and requesting permission with an dialog if not given and then enabling casting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import GoogleCast
-
+import Network
 
 
 @UIApplicationMain
@@ -24,7 +24,33 @@ import GoogleCast
     GCKLogger.sharedInstance().delegate = self 
     // End Cast Code 
     // GeneratedPluginRegistrant.register(with: self)
+    //For LNA channel
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let channel = FlutterMethodChannel(name: "local_network_authorization", binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler({
+          [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          switch call.method {
+            case "requestLocalNetworkAuthorization":
+                self?.requestLocalNetworkAuthorization(completion: result)
+          default:
+              result(FlutterMethodNotImplemented)
+          }
+      })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      
+
+  }
+
+  private func requestLocalNetworkAuthorization(completion: @escaping FlutterResult) {
+    // Create parameters, and allow browsing over peer-to-peer link.
+        // Create a LocalNetworkAuthorization instance.
+    let localNetworkAuthorization = LocalNetworkAuthorization()
+    
+    // Request local network authorization using the instance.
+    localNetworkAuthorization.requestAuthorization(completion: completion)
   }
     
     // private func startNodle(result: FlutterResult) {
@@ -33,3 +59,62 @@ import GoogleCast
 
 }
 
+
+// Call check
+@available(iOS 14.0, *)
+public class LocalNetworkAuthorization: NSObject {
+    private var browser: NWBrowser?
+    private var netService: NetService?
+    private var completion: ((Bool) -> Void)?
+    
+    public func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        print("Called requestAuthorization");
+        self.completion = completion
+        
+        // Create parameters, and allow browsing over peer-to-peer link.
+        let parameters = NWParameters()
+        parameters.includePeerToPeer = true
+        
+        // Browse for a custom service type.
+        let browser = NWBrowser(for: .bonjour(type: "_bonjour._tcp", domain: nil), using: parameters)
+        print("Browsing for service type")
+        self.browser = browser
+        browser.stateUpdateHandler = { newState in
+            switch newState {
+            case .failed(let error):
+                print(error.localizedDescription)
+            case .ready, .cancelled:
+                break
+            case let .waiting(error):
+                print("Local network permission has been denied: \(error)")
+                self.reset()
+                self.completion?(false)
+            default:
+                break
+            }
+        }
+         print("Starting NetService")
+        self.netService = NetService(domain: "local.", type:"_lnp._tcp.", name: "LocalNetworkPrivacy", port: 1100)
+        self.netService?.delegate = self
+        
+        self.browser?.start(queue: .main)
+        self.netService?.publish()
+        print("NetService Published")
+    }
+    
+    private func reset() {
+        self.browser?.cancel()
+        self.browser = nil
+        self.netService?.stop()
+        self.netService = nil
+    }
+}
+
+@available(iOS 14.0, *)
+extension LocalNetworkAuthorization : NetServiceDelegate {
+    public func netServiceDidPublish(_ sender: NetService) {
+        self.reset()
+        print("Local network permission has been granted")
+        completion?(true)
+    }
+}
--- lib/src/modules/chase_view/view/parts/video_top_actions.dart
+++ lib/src/modules/chase_view/view/parts/video_top_actions.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_video_cast/flutter_video_cast.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import '../../../../const/sizings.dart';
+import '../../../../core/top_level_providers/services_providers.dart';
 import 'animations_overlay_toggle_switch.dart';
 import 'mp4_player/mp4_player.dart';
 
@@ -59,48 +64,250 @@ class _VideoTopActionsState extends State<VideoTopActions> {
   }
 }
 
-class ChaseAppChromeCastButton extends StatefulWidget {
+final AutoDisposeFutureProvider<bool?> localNetworkAccessStatusFutureProvider =
+    FutureProvider.autoDispose<bool?>(
+  (AutoDisposeFutureProviderRef<bool?> ref) async {
+    final bool isShownPermissionDialog = ref
+            .read(sharedPreferancesProvider)
+            .getBool('isShownLANPermissionDialog') ??
+        false;
+
+    return isShownPermissionDialog
+        ? await LocalNetworkAuthorization().requestAuthorization()
+        : isShownPermissionDialog;
+  },
+);
+
+class ChaseAppChromeCastButton extends ConsumerStatefulWidget {
   const ChaseAppChromeCastButton({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<ChaseAppChromeCastButton> createState() =>
+  ConsumerState<ChaseAppChromeCastButton> createState() =>
       _ChaseAppChromeCastButtonState();
 }
 
-class _ChaseAppChromeCastButtonState extends State<ChaseAppChromeCastButton> {
+class _ChaseAppChromeCastButtonState
+    extends ConsumerState<ChaseAppChromeCastButton>
+    with WidgetsBindingObserver {
   late ChromeCastController _controller;
 
   @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(kBorderRadiusStandard),
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          ChromeCastButton(
-            size: kIconSizeLargeConstant,
-            color: Colors.red,
-            onButtonCreated: (ChromeCastController controller) {
-              setState(() => _controller = controller);
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
 
-              _controller.addSessionListener();
-            },
-            onSessionStarted: () {
-              _controller.loadMedia(
-                'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  // add didChangeLifeCycle call
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      ref.refresh(localNetworkAccessStatusFutureProvider);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AsyncValue<bool?> state =
+        ref.watch(localNetworkAccessStatusFutureProvider);
+
+    return state.when(
+      data: (bool? data) {
+        return data == true
+            ? DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(kBorderRadiusStandard),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    ChromeCastButton(
+                      size: kIconSizeLargeConstant,
+                      color: Colors.red,
+                      onButtonCreated: (ChromeCastController controller) {
+                        setState(() => _controller = controller);
+
+                        _controller.addSessionListener();
+                      },
+                      onSessionStarted: () {
+                        _controller.loadMedia(
+                          'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+                        );
+                      },
+                    ),
+                    const IgnorePointer(
+                      child: Icon(Icons.cast),
+                    ),
+                  ],
+                ),
+              )
+            : IconButton(
+                onPressed: () async {
+                  final bool? isAccepted = await showCupertinoDialog<bool?>(
+                    context: context,
+                    builder: (BuildContext context) => CupertinoAlertDialog(
+                      title: Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.onBackground,
+                            child: Icon(
+                              Icons.cast,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: kItemsSpacingSmallConstant,
+                          ),
+                          const Flexible(
+                            child: Text(
+                              'Casting Permission',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      content: const Text(
+                        'To use casting feature, we need to access your local network to connect to your casting device. Please allow the permission when asked at next step.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          style: OutlinedButton.styleFrom(
+                            padding: const EdgeInsets.all(kButtonPaddingMedium),
+                          ),
+                          onPressed: () async {
+                            Navigator.pop(context, true);
+                          },
+                          child: const Text(
+                            'Enable',
+                            style: TextStyle(
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: kPaddingSmallConstant,
+                        ),
+                        TextButton(
+                          style: OutlinedButton.styleFrom(
+                            padding: const EdgeInsets.all(kButtonPaddingMedium),
+                          ),
+                          onPressed: () async {
+                            Navigator.pop(context, false);
+                          },
+                          child: Text(
+                            'Later',
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                  if (isAccepted == true) {
+                    try {
+                      await ref
+                          .read(sharedPreferancesProvider)
+                          .setBool('isShownLANPermissionDialog', true);
+
+                      final bool? permission = await LocalNetworkAuthorization()
+                          .requestAuthorization();
+
+                      if (permission != null) {
+                        if (permission) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text(
+                                'Casting Enabled',
+                              ),
+                              action: SnackBarAction(
+                                label: 'Open Settings',
+                                onPressed: () async {
+                                  await openAppSettings();
+                                },
+                              ),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text(
+                                'Update permissions later in app settings.',
+                              ),
+                              action: SnackBarAction(
+                                label: 'Open Settings',
+                                onPressed: () async {
+                                  await openAppSettings();
+                                },
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                      // final String? deviceIp = await NetworkInfo().getWifiIP();
+
+                      // const Duration timeOutDuration =
+                      //     Duration(milliseconds: 100);
+                      // await Socket.connect(
+                      //   deviceIp,
+                      //   80,
+                      //   timeout: timeOutDuration,
+                      // );
+                    } catch (e) {
+                      // if (permissionStatus != null && !permissionStatus) {
+                      //   await openAppSettings();
+                      // }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text(
+                            'Something went wrong. Try updating permission in app settings.',
+                          ),
+                          action: SnackBarAction(
+                            label: 'Open Settings',
+                            onPressed: () async {
+                              await openAppSettings();
+                            },
+                          ),
+                        ),
+                      );
+                    } finally {
+                      ref.refresh(localNetworkAccessStatusFutureProvider);
+                    }
+                  }
+                  ref.refresh(localNetworkAccessStatusFutureProvider);
+                },
+                icon: const Icon(Icons.cast),
               );
-            },
-          ),
-          const IgnorePointer(
-            child: Icon(Icons.cast),
-          ),
-        ],
-      ),
+      },
+      loading: () => const SizedBox.shrink(),
+      error: (Object error, StackTrace? stack) => const SizedBox.shrink(),
     );
+  }
+}
+
+class LocalNetworkAuthorization {
+  static const MethodChannel _channel =
+      MethodChannel('local_network_authorization');
+
+  Future<bool?> requestAuthorization() async {
+    // Request permission to access the local network.
+    final bool? isPermissionGranted =
+        await _channel.invokeMethod<bool?>('requestLocalNetworkAuthorization');
+
+    // Return the result of the permission request.
+    return isPermissionGranted;
   }
 }
