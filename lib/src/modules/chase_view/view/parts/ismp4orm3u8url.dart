549113c59c5b3ce6399f85b23bfe43623d027680
rutvik110 <takrutvik@gmail.com>
2022-12-12 22:03:01 +0530

    refactor and improved media url checkers

--- lib/src/modules/chase_view/view/parts/ismp4orm3u8url.dart
+++ lib/src/modules/chase_view/view/parts/ismp4orm3u8url.dart
@@ -1,8 +1,1 @@
-import 'package:path/path.dart' as path;
 
-bool ismp4orm3u8url(String url) {
-  final String extension = path.extension(url.split('?').first);
-  final bool isMp4 = extension == '.mp4' || extension == '.m3u8';
-
-  return isMp4;
-}
